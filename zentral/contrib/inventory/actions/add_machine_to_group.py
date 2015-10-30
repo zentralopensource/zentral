018dcd661fe5d7839c0888b2fa5b941cd8fd097c
Éric Falconnier <eric.falconnier@112hz.com>
2015-10-30 12:52:03 +0100

    Fix add_machine_to_group action.

--- zentral/contrib/inventory/actions/add_machine_to_group.py
+++ zentral/contrib/inventory/actions/add_machine_to_group.py
@@ -8,9 +8,9 @@ class Action(object):
     def __init__(self, config_d):
         self.config_d = config_d
 
-    def trigger(self, event_d, action_config_d, probe=None, query_conf=None):
+    def trigger(self, event, action_config_d):
         group_name = action_config_d['group_name']
         try:
-            inventory.add_machine_to_group(event_d['_zentral']['machine'], group_name)
+            inventory.add_machine_to_group(event.machine, group_name)
         except:
             logger.exception('Could not add machine to group %s', group_name)
