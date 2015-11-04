37723fcd3126116c53f9d5b536dcbcd32efeb4b7
Éric Falconnier <eric.falconnier@112hz.com>
2015-11-04 17:52:51 +0100

    Add pushover action backend
    https://pushover.net/

---
+++ zentral/core/actions/backends/pushover.py
@@ -0,0 +1,19 @@
+import json
+import requests
+from .base import BaseAction
+from zentral.conf import contact_groups
+
+API_ENDPOINT = "https://api.pushover.net/1/messages.json"
+
+
+class Action(BaseAction):
+    def trigger(self, event, action_config_d):
+        action_config_d = action_config_d or {}
+        args = {'message': '\n\n'.join([event.get_notification_subject(), event.get_notification_body()]),
+                'token': self.config_d['token'],}
+        for group_name in action_config_d['groups']:
+            for contact_d in contact_groups[group_name]:
+                pushover_user_token = contact_d.get('pushover_user_token', None)
+                if pushover_user_token:
+                    args['user'] = pushover_user_token
+                    requests.post(API_ENDPOINT, data=args)
