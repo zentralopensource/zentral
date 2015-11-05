4f8bf10a0a15b6d86cc64b4e8598aabeec89394b
Éric Falconnier <eric.falconnier@112hz.com>
2015-11-05 13:50:00 +0100

    flake8 on the pushover action.

--- zentral/core/actions/backends/pushover.py
+++ zentral/core/actions/backends/pushover.py
@@ -10,7 +10,7 @@ class Action(BaseAction):
     def trigger(self, event, action_config_d):
         action_config_d = action_config_d or {}
         args = {'message': '\n\n'.join([event.get_notification_subject(), event.get_notification_body()]),
-                'token': self.config_d['token'],}
+                'token': self.config_d['token']}
         for group_name in action_config_d['groups']:
             for contact_d in contact_groups[group_name]:
                 pushover_user_token = contact_d.get('pushover_user_token', None)
