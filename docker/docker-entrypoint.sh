91ccd8decaebf8db245a3ccb20e5f284c9cc8d29
groob <vrancean+github@gmail.com>
2015-11-07 02:39:19 -0500

    change entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,8 +6,7 @@ echo
 /usr/bin/python3.4 /home/zentral/zentral/bin/check_configuration.py
 if [ "$?" -eq 0 ] && [ "$1" = 'run' ]
 then
-  echo
-  echo 'Launch supervisor'
-  echo
-  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+    python3 /home/zentral/server/manage.py migrate
 fi
+
+exec "$@"
