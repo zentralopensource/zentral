19611fd01f43199c69646ca63cc9f86888d781a9
headmin <head@mb.local>
2015-11-09 14:44:18 +0100

    fix for twilio api POST

--- zentral/core/actions/backends/twilio.py
+++ zentral/core/actions/backends/twilio.py
@@ -10,8 +10,8 @@ class Action(BaseAction):
     def __init__(self, config_d):
         super(Action, self).__init__(config_d)
         self.auth = (config_d['account_sid'], config_d['auth_token'])
-        self.url = "{api_endpoint}{account_sid}/Messages".format(api_endpoint=API_ENDPOINT,
-                                                                 account_sid=config_d['account_sid'])
+        self.url = "{api_endpoint}Accounts/{account_sid}/Messages".format(api_endpoint=API_ENDPOINT,
+                                                                          account_sid=config_d['account_sid'])
 
     def trigger(self, event, action_config_d):
         action_config_d = action_config_d or {}
