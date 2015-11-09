29ac3c26a8ba4a611a220f8f949dfbf25bee1d01
headmin <head@mb.local>
2015-11-09 14:24:31 +0100

    add sms notification via twillo

---
+++ zentral/core/actions/backends/twilio.py
@@ -0,0 +1,21 @@
+import json
+import requests
+from .base import BaseAction
+
+API_ENDPOINT = "https://api.twilio.com/2010-04-01"
+
+
+class Action(BaseAction):
+    def __init__(self, config_d):
+        super(Action, self).__init__(config_d)
+        self.auth = (config_d['account_sid'], config_d['auth_token'])
+
+    def trigger(self, event, action_config_d):
+        action_config_d = action_config_d or {}
+        args = {'Body': '\n\n'.join([event.get_notification_subject(), event.get_notification_body()])}
+        for group_name in action_config_d['groups']:
+            for contact_d in contact_groups[group_name]:
+                cell_number = contact_d.get('cell', None)
+                if cell_number:
+                    args['To'] =  cell_number
+                    requests.post(API_ENDPOINT, data=args, auth=self.auth)
