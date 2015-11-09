a6ffdbfed3fe326879ed263f06e00a6321695dd4
headmin <head@mb.local>
2015-11-09 14:41:25 +0100

    fix url scheme for twiliio API

--- zentral/core/actions/backends/twilio.py
+++ zentral/core/actions/backends/twilio.py
@@ -1,14 +1,17 @@
 import json
 import requests
 from .base import BaseAction
+from zentral.conf import contact_groups
 
-API_ENDPOINT = "https://api.twilio.com/2010-04-01"
+API_ENDPOINT = "https://api.twilio.com/2010-04-01/"
 
 
 class Action(BaseAction):
     def __init__(self, config_d):
         super(Action, self).__init__(config_d)
         self.auth = (config_d['account_sid'], config_d['auth_token'])
+        self.url = "{api_endpoint}{account_sid}/Messages".format(api_endpoint=API_ENDPOINT,
+                                                                 account_sid=config_d['account_sid'])
 
     def trigger(self, event, action_config_d):
         action_config_d = action_config_d or {}
@@ -18,4 +21,4 @@ class Action(BaseAction):
                 cell_number = contact_d.get('cell', None)
                 if cell_number:
                     args['To'] =  cell_number
-                    requests.post(API_ENDPOINT, data=args, auth=self.auth)
+                    requests.post(self.url, data=args, auth=self.auth)
