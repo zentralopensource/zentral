5436660138ccbe5350a2772d30a765200c67ae9e
Éric Falconnier <eric.falconnier@112hz.com>
2015-11-11 16:18:59 +0100

    Fix all in one docker container django conf.

--- docker/settings.py
+++ docker/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
-    'configuration',
+    'base',
     'zentral.contrib.inventory',
     'zentral.contrib.osquery',
     'zentral.contrib.santa',
