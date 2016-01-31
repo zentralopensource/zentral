931e0e592a65d76a9c95d06d39a9e3ee43e560e6
Éric Falconnier <eric.falconnier@112hz.com>
2016-01-31 22:17:01 +0100

    Better query pack conversion.

--- zentral/bin/query_pack_to_probe.py
+++ zentral/bin/query_pack_to_probe.py
@@ -2,11 +2,9 @@ import argparse
 import json
 import os
 import sys
-ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), "../../"))
-sys.path.insert(0, ROOT_DIR)
 
 
-def build_probe(pack_filepath, probe_filepath):
+def build_probe(pack_filepath, probe_filepath, probe_actions):
     if os.path.exists(probe_filepath):
         raise ValueError('Probe file already exist')
     f = open(pack_filepath, 'r')
@@ -14,8 +12,10 @@ def build_probe(pack_filepath, probe_filepath):
     f.close()
     probe_d = {"name": os.path.basename(pack_filepath.rstrip('.conf')),
                "osquery": {"schedule": []},
-               "actions": {"slack_macops": {}}}
+               "actions": json.loads(probe_actions)}
     for q_name, q_conf in pack_d['queries'].items():
+        value = q_conf.get("value", "")
+        q_conf["value"] = "{}: {}".format(q_name, value)
         probe_d['osquery']['schedule'].append(q_conf)
     f = open(probe_filepath, 'w')
     json.dump(probe_d, f, indent=2)
@@ -26,5 +26,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build a probe from a query pack')
     parser.add_argument('--pack', metavar='PCK', help="a query pack", required=True)
     parser.add_argument('--probe', metavar='PRB', help="a probe file", required=True)
+    parser.add_argument('--actions', metavar='ACT', help="probe actions json cfg", required=True)
     args = parser.parse_args()
-    build_probe(args.pack, args.probe)
+    build_probe(args.pack, args.probe, args.actions)
