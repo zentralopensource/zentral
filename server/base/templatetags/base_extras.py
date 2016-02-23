efa875c377b057e8adcfaf18d8770d7c5a13a930
Éric Falconnier <eric.falconnier@112hz.com>
2016-02-23 19:44:30 +0100

    Fix active dropdown bug.

--- server/base/templatetags/base_extras.py
+++ server/base/templatetags/base_extras.py
@@ -21,8 +21,8 @@ def main_menu_app_dropdowns(context):
             if not main_menu_cfg:
                 logger.warning('App %s w/o main menu config', app_name)
                 continue
-            dropdown_cfg = {'title': main_menu_cfg.get('title', None) or app_shortname.title(),
-                            'is_active': context.get(app_shortname, False) is True,
+            dropdown_cfg = {'app_shortname': app_shortname,
+                            'title': main_menu_cfg.get('title', None) or app_shortname.title(),
                             'link_list': []}
             for url_name, anchor_text in main_menu_cfg['items']:
                 dropdown_cfg['link_list'].append((reverse('{}:{}'.format(app_shortname, url_name)),
@@ -30,5 +30,7 @@ def main_menu_app_dropdowns(context):
             if dropdown_cfg['link_list']:
                 dropdown_cfg['main_link'] = dropdown_cfg['link_list'][0][0]
             DROPDOWN_LIST.append(dropdown_cfg)
+    for dropdown_cfg in DROPDOWN_LIST:
+        dropdown_cfg['is_active'] = context.get(dropdown_cfg['app_shortname'], False) is True
     context['dropdown_list'] = DROPDOWN_LIST
     return context
