a281eb716c79e218f1bf12e50de5eb20d74db476
Éric Falconnier <eric.falconnier@112hz.com>
2016-09-29 18:42:32 +0200

    Add default tags for the zendesk ticket action
    Strings of comma separated tags or tag arrays are accepted in the
    action default configuration and in the action probe configuration.

--- zentral/contrib/zendesk/actions/ticket.py
+++ zentral/contrib/zendesk/actions/ticket.py
@@ -10,10 +10,19 @@ API_ENDPOINT = "https://{subdomain}.zendesk.com/api/v2/tickets.json"
 
 
 class Action(BaseAction):
+    def _prepare_tag_set(self, tags):
+        if isinstance(tags, str):
+            tags = [t.strip() for t in tags.split(',') if t.strip()]
+        if not isinstance(tags, list):
+            logger.error('Wrong tag configuration for action %s', self.name)
+            tags = []
+        return set(tags)
+
     def __init__(self, config_d):
         super(Action, self).__init__(config_d)
         self.auth = ('{email}/token'.format(**config_d), config_d['token'])
         self.url = API_ENDPOINT.format(**config_d)
+        self.default_tag_set = self._prepare_tag_set(config_d.get('tags', []))
 
     def trigger(self, event, probe, action_config_d):
         action_config_d = action_config_d or {}
@@ -28,9 +37,10 @@ class Action(BaseAction):
         args['ticket']['priority'] = priority
 
         # tags
-        tags = action_config_d.get('tags', None)
-        if tags:
-            args['ticket']['tags'] = tags
+        action_tag_set = self._prepare_tag_set(action_config_d.get('tags', []))
+        tag_set = action_tag_set | self.default_tag_set
+        if tag_set:
+            args['ticket']['tags'] = list(tag_set)
 
         r = requests.post(self.url, headers={'Content-Type': 'application/json'},
                           data=json.dumps(args), auth=self.auth)
