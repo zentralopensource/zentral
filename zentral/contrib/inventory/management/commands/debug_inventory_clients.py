72cebd8684c83123ba81a00651acd8a8199a1210
Éric Falconnier <eric.falconnier@112hz.com>
2016-12-15 15:54:16 +0100

    Better options for the debug_inventory_clients cmd

--- zentral/contrib/inventory/management/commands/debug_inventory_clients.py
+++ zentral/contrib/inventory/management/commands/debug_inventory_clients.py
@@ -10,22 +10,33 @@ class Command(BaseCommand):
     help = "Debug inventory clients"
 
     def add_arguments(self, parser):
-        parser.add_argument('--list', action='store_true', dest='list_client', default=False, help='list clients')
-        parser.add_argument('--get-machines', dest='client_id', type=int, nargs=1, help='get client machines')
+        parser.add_argument('--list-clients', action='store_true', dest='list_clients', default=False,
+                            help='list clients')
+        parser.add_argument('--client', dest='client_id', type=int, nargs=1,
+                            help='get client machines')
+        parser.add_argument('--serial-number', dest='serial_number', type=str, nargs=1,
+                            help='get client machine by serial_number')
 
     def handle(self, *args, **kwargs):
-        if kwargs.get("list_client"):
+        if kwargs.get("list_clients"):
             print("Configured clients:")
             for idx, client in enumerate(clients):
                 print(idx)
                 for key, val in client.source.items():
                     print(key, val)
         client_id = kwargs.get("client_id")
+        serial_number = kwargs.get("serial_number")
         if client_id:
             client_id = client_id[0]
+            if serial_number:
+                serial_number = serial_number[0]
             try:
                 client = clients[client_id]
             except IndexError:
                 raise CommandError("Client {} does not exist".format(client_id))
+            n = 0
             for tree in client.get_machines():
-                pprint.pprint(tree)
+                n += 1
+                if serial_number is None or tree.get("serial_number") == serial_number:
+                    pprint.pprint(tree)
+            print(n, "MACHINES")
