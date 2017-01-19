218d97f303134414006cf53e18d12d3b99185791
Éric Falconnier <eric.falconnier@112hz.com>
2017-01-19 14:39:49 +0100

    Fix create event probe view title

--- zentral/core/probes/views.py
+++ zentral/core/probes/views.py
@@ -71,6 +71,7 @@ class CreateProbeView(LoginRequiredMixin, FormView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx['probes'] = True
+        ctx['title'] = "Create event probe"
         return ctx
 
     def form_valid(self, form):
