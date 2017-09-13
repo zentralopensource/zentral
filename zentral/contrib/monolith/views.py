zentralopensource/zentral

commit ce338c7fd8c558cf49ee7f86726f7f6adbf8445e

Fix errors when enrollement packages are missing
The enrollment packages are built when the manifest is edited, but it
could be that they are not there anymore (multiple web servers without a
common object store for the packages, MEDIA_ROOT deleted, …)

zentral/contrib/monolith/views.py
@@ -944,6 +944,9 @@ class MRPackageView(MRSignedView):
                 mep = ManifestEnrollmentPackage.objects.get(manifest=self.manifest, pk=mep_id)
             except ManifestEnrollmentPackage.DoesNotExist:
                 return
+            if not os.path.exists(mep.file.name):
+                logger.error("Missing enrollment package %s", mep.file.name)
+                build_manifest_enrollment_package(mep)
             event_payload["manifest_enrollment_package"]["filename"] = mep.file.name
             return FileResponse(mep.file)
         elif model == "sub_manifest_attachment":

commit 28a419ed0570ed4cb26337744adccf7475562540

Fix monolith enrollment packages
The previous commit ce338c7fd8c introduced a bug. It is not possible to
build the enrollment package when they are downloaded if they are
missing. Their checksum would be different than the one present in the
pkg_info when the catalog was built, and munki would reject them. (The
checksum is updated by build_manifest_enrollment_package, but it is too
late.)

The rebuild_manifest_enrollment_packages command should be used instead,
if some packages are missing.

We also fix a bug in the osquery enrollment package. When we added the
option to disable or enable the file carver, we forgot the previously
configured osquery enrollment packages which didn't have the option. We
set it to "disabled" per default, like in the enrollment package form.

zentral/contrib/monolith/views.py
@@ -944,9 +944,6 @@ class MRPackageView(MRSignedView):
                 mep = ManifestEnrollmentPackage.objects.get(manifest=self.manifest, pk=mep_id)
             except ManifestEnrollmentPackage.DoesNotExist:
                 return
-            if not os.path.exists(mep.file.name):
-                logger.error("Missing enrollment package %s", mep.file.name)
-                build_manifest_enrollment_package(mep)
             event_payload["manifest_enrollment_package"]["filename"] = mep.file.name
             return FileResponse(mep.file)
         elif model == "sub_manifest_attachment":

zentral/contrib/osquery/osx_package/builder.py
@@ -99,7 +99,7 @@ class OsqueryZentralEnrollPkgBuilder(PackageBuilder):
         extra_prog_args.append("--tls_server_certs={}".format(tls_server_certs_install_path))
 
         # carver
-        disable_carver = kwargs["disable_carver"]
+        disable_carver = kwargs.get("disable_carver", True)
         extra_prog_args.append("--disable_carver={}".format(str(disable_carver).lower()))
         if not disable_carver:
             extra_prog_args.append("--carver_start_endpoint={}".format(reverse('osquery:carver_start')))