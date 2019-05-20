0bd7bb8f8cceff0ac20d2f9974771adf6a2bcaf7
Éric Falconnier <eric.falconnier@112hz.com>
2019-05-20 19:36:23 +0200

    Add a S/N cache in xnumon preprocessor

--- zentral/contrib/xnumon/preprocessors.py
+++ zentral/contrib/xnumon/preprocessors.py
@@ -1,12 +1,23 @@
-from dateutil import parser
+from functools import lru_cache
 import json
 import logging
+from dateutil import parser
 from zentral.utils.certificates import parse_dn
 from zentral.contrib.filebeat.models import EnrollmentSession
 from .events import XnumonLogEvent
 
 
 logger = logging.getLogger("zentral.contrib.xnumon.preprocessors")
+
+
+@lru_cache(maxsize=32)
+def get_serial_number(secret):
+    try:
+        return (EnrollmentSession.objects.select_related("enrollment_secret")
+                                         .get(enrollment_secret__secret=secret)
+                                         .enrollment_secret.serial_numbers[0])
+    except (EnrollmentSession.DoesNotExist, IndexError):
+        pass
 
 
 class XnumonLogPreprocessor(object):
@@ -17,10 +28,10 @@ class XnumonLogPreprocessor(object):
             raw_event_d = json.loads(raw_event)
             tls_peer = json.loads(raw_event_d.pop("tls_peer"))
             subject = parse_dn(tls_peer["subject"])
-            subject_prefix, secret = subject["CN"].split("$")
-            enrollment_session = (EnrollmentSession.objects.select_related("enrollment_secret")
-                                                           .get(enrollment_secret__secret=secret))
-            serial_number = enrollment_session.enrollment_secret.serial_numbers[0]
+            _, secret = subject["CN"].split("$")
+            serial_number = get_serial_number(secret)
+            if not serial_number:
+                return
             event_data = raw_event_d["json"]
             user_agent = "/".join(raw_event_d.get("agent", {}).get(attr) for attr in ("type", "version"))
             ip_address = raw_event_d.get("filebeat_ip_address")
