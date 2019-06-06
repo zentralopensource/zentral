15966ee71d7f3a97f85dcab5c7a85d5ef864e2f5
Éric Falconnier <eric.falconnier@112hz.com>
2019-06-06 12:30:29 +0200

    Different event classes for xnumon eventcode

--- zentral/contrib/xnumon/events/__init__.py
+++ zentral/contrib/xnumon/events/__init__.py
@@ -7,9 +7,37 @@ logger = logging.getLogger('zentral.contrib.xnumon.events')
 ALL_EVENTS_SEARCH_DICT = {"tag": "xnumon"}
 
 
-class XnumonLogEvent(BaseEvent):
-    event_type = "xnumon_log"
+class XnumonOpsEvent(BaseEvent):
+    event_type = "xnumon_ops"
     tags = ["xnumon"]
+    xnumon_eventcode = 0
 
 
-register_event_type(XnumonLogEvent)
+register_event_type(XnumonOpsEvent)
+
+
+class XnumonImageExecEvent(BaseEvent):
+    event_type = "xnumon_image_exec"
+    tags = ["xnumon"]
+    xnumon_eventcode = 2
+
+
+register_event_type(XnumonImageExecEvent)
+
+
+class XnumonProcessAccessEvent(BaseEvent):
+    event_type = "xnumon_process_access"
+    tags = ["xnumon"]
+    xnumon_eventcode = 3
+
+
+register_event_type(XnumonProcessAccessEvent)
+
+
+class XnumonLaunchdAddEvent(BaseEvent):
+    event_type = "xnumon_launchd_add"
+    tags = ["xnumon"]
+    xnumon_eventcode = 4
+
+
+register_event_type(XnumonLaunchdAddEvent)
--- zentral/contrib/xnumon/preprocessors.py
+++ zentral/contrib/xnumon/preprocessors.py
@@ -2,7 +2,7 @@ import json
 import logging
 from dateutil import parser
 from zentral.contrib.filebeat.utils import get_serial_number_from_raw_event
-from .events import XnumonLogEvent
+from .events import XnumonImageExecEvent, XnumonLaunchdAddEvent, XnumonOpsEvent, XnumonProcessAccessEvent
 
 
 logger = logging.getLogger("zentral.contrib.xnumon.preprocessors")
@@ -10,6 +10,11 @@ logger = logging.getLogger("zentral.contrib.xnumon.preprocessors")
 
 class XnumonLogPreprocessor(object):
     routing_key = "xnumon_logs"
+    eventcode_mapping = dict((event_class.xnumon_eventcode, event_class)
+                             for event_class in (XnumonOpsEvent,
+                                                 XnumonImageExecEvent,
+                                                 XnumonProcessAccessEvent,
+                                                 XnumonLaunchdAddEvent))
 
     def process_raw_event(self, raw_event):
         try:
@@ -20,10 +25,11 @@ class XnumonLogPreprocessor(object):
             event_data = raw_event_d["json"]
             user_agent = "/".join(raw_event_d.get("agent", {}).get(attr) for attr in ("type", "version"))
             ip_address = raw_event_d.get("filebeat_ip_address")
+            event_class = self.eventcode_mapping[int(event_data.pop("eventcode"))]
         except Exception:
             logger.exception("Could not process xnumon_log raw event")
         else:
-            yield from XnumonLogEvent.build_from_machine_request_payloads(
+            yield from event_class.build_from_machine_request_payloads(
                 serial_number, user_agent, ip_address, [event_data],
                 get_created_at=lambda d: parser.parse(d.pop("time"))
             )
