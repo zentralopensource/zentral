3e6857ca55a936c161c87de96f24b64409879ea1
Éric Falconnier <eric.falconnier@112hz.com>
2019-09-18 17:24:32 +0200

    Better filebeat event utils
    Make sure that it works for external client certs
    Use the EnrolledMachine to find the serial number

--- zentral/contrib/filebeat/utils.py
+++ zentral/contrib/filebeat/utils.py
@@ -1,33 +1,42 @@
 import json
 import logging
 from zentral.utils.certificates import parse_dn
-from .models import EnrollmentSession
+from .models import EnrolledMachine
 
 
 logger = logging.getLogger("zentral.contrib.filebeat.utils")
 
 
-def get_serial_number_from_enrollment_secret(secret):
-    # TODO optimization. Could be cached. With encrypted communications to the cache server (like with the DB) !
-    try:
-        return (EnrollmentSession.objects.select_related("enrollment_secret")
-                                         .get(enrollment_secret__secret=secret)
-                                         .enrollment_secret.serial_numbers[0])
-    except (EnrollmentSession.DoesNotExist, IndexError):
-        pass
-
-
-def get_serial_number_from_raw_event(raw_event_d):
+def get_tls_peer_identifiers_from_raw_event(raw_event_d):
+    serial_number = enrollment_secret_secret = None
     try:
         tls_peer = json.loads(raw_event_d.pop("tls_peer"))
         subject = parse_dn(tls_peer["subject"])
         serial_number = subject.get("serialNumber")
-        if not serial_number:
-            _, secret = subject["CN"].split("$")
-            serial_number = get_serial_number_from_enrollment_secret(secret)
-        return serial_number
     except Exception:
-        logger.exception("Could not extract the S/N from the raw event")
+        logger.exception("Could not extract identifiers from the raw event")
+    else:
+        try:
+            cn_prefix, cn_secret = subject["CN"].split("$")
+        except Exception:
+            pass
+        else:
+            if cn_prefix == "FLBT":  # TODO define in one place
+                enrollment_secret_secret = cn_secret
+    return serial_number, enrollment_secret_secret
+
+
+def get_serial_number_from_raw_event(raw_event_d):
+    serial_number, secret = get_tls_peer_identifiers_from_raw_event(raw_event_d)
+    if not serial_number and secret:
+        # TODO: optimization: cache
+        try:
+            serial_number = EnrolledMachine.objects.get(
+                enrollment_session__enrollment_secret__secret=secret
+            ).serial_number
+        except EnrolledMachine.DoesNotExist:
+            pass
+    return serial_number
 
 
 def get_user_agent_and_ip_address_from_raw_event(raw_event_d):
