a339c5459d81fcd7a66bbd60f7722daa7601c6b0
Éric Falconnier <eric.falconnier@112hz.com>
2020-05-14 10:17:54 +0200

    convert accounts urls to paths

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,27 +1,27 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = "users"
 urlpatterns = [
-    url(r'^$', views.UsersView.as_view(),
-        name="list"),
-    url(r'^nginx/auth_request/$', views.NginxAuthRequestView.as_view(),
-        name="nginx_auth_request"),
-    url(r'^add/$', views.AddUserView.as_view(),
-        name="add"),
-    url(r'^(?P<pk>[0-9]+)/update/$', views.UpdateUserView.as_view(),
-        name="update"),
-    url(r'^(?P<pk>[0-9]+)/delete/$', views.DeleteUserView.as_view(),
-        name="delete"),
-    url(r'^verification_devices/$', views.UserVerificationDevicesView.as_view(),
-        name="verification_devices"),
-    url(r'^add_totp/$', views.AddTOTPView.as_view(),
-        name="add_totp"),
-    url(r'^totp/(?P<pk>[0-9]+)/delete/$', views.DeleteTOTPView.as_view(),
-        name="delete_totp"),
-    url(r'^u2f_devices/register/$', views.RegisterU2FDeviceView.as_view(),
-        name="register_u2f_device"),
-    url(r'^u2f_devices/(?P<pk>[0-9]+)/delete/$', views.DeleteU2FDeviceView.as_view(),
-        name="delete_u2f_device"),
+    path('', views.UsersView.as_view(),
+         name="list"),
+    path('nginx/auth_request/', views.NginxAuthRequestView.as_view(),
+         name="nginx_auth_request"),
+    path('add/', views.AddUserView.as_view(),
+         name="add"),
+    path('<int:pk>/update/', views.UpdateUserView.as_view(),
+         name="update"),
+    path('<int:pk>/delete/', views.DeleteUserView.as_view(),
+         name="delete"),
+    path('verification_devices/', views.UserVerificationDevicesView.as_view(),
+         name="verification_devices"),
+    path('add_totp/', views.AddTOTPView.as_view(),
+         name="add_totp"),
+    path('totp/<int:pk>/delete/', views.DeleteTOTPView.as_view(),
+         name="delete_totp"),
+    path('u2f_devices/register/', views.RegisterU2FDeviceView.as_view(),
+         name="register_u2f_device"),
+    path('u2f_devices/<int:pk>/delete/', views.DeleteU2FDeviceView.as_view(),
+         name="delete_u2f_device"),
 ]
