be61e71e2627ada03ebe80b88c11c37b2d6d549c
Éric Falconnier <eric.falconnier@112hz.com>
2020-08-25 19:32:44 +0200

    Add MachineSnapshots to jamf_protect

--- zentral/contrib/jamf_protect/views.py
+++ zentral/contrib/jamf_protect/views.py
@@ -9,8 +9,9 @@ from django.views.generic import View, ListView, TemplateView
 from zentral.conf import settings
 from zentral.contrib.inventory.exceptions import EnrollmentSecretVerificationFailed
 from zentral.contrib.inventory.forms import EnrollmentSecretForm
-from zentral.contrib.inventory.models import MachineTag
-from zentral.contrib.inventory.utils import verify_enrollment_secret
+from zentral.contrib.inventory.models import MachineTag, MetaMachine
+from zentral.contrib.inventory.utils import (commit_machine_snapshot_and_trigger_events,
+                                             verify_enrollment_secret)
 from zentral.utils.http import user_agent_and_ip_address_from_request
 from .events import post_enrollment_event, post_event
 from .models import EnrolledMachine, Enrollment
@@ -75,7 +76,7 @@ class CreateEnrollmentView(LoginRequiredMixin, TemplateView):
 
 
 class PostEventView(View):
-    def get_token(self, request):
+    def load_token(self, request):
         try:
             header = request.META['HTTP_AUTHORIZATION']
         except KeyError:
@@ -84,10 +85,9 @@ class PostEventView(View):
             raise PermissionDenied("Empty authorization header")
         if not header.startswith("Bearer"):
             raise PermissionDenied("Invalid authorization header")
-        token = header.replace("Bearer", "").strip()
-        if not token:
+        self.token = header.replace("Bearer", "").strip()
+        if not self.token:
             raise PermissionDenied("Empty Bearer token")
-        return token
 
     def load_event(self, request):
         try:
@@ -95,38 +95,66 @@ class PostEventView(View):
         except Exception:
             raise SuspiciousOperation("Could not load request body")
         try:
-            event = payload["input"]
-            serial_number = event["host"]["serial"]
+            self.event = payload["input"]
+            self.serial_number = self.event["host"]["serial"]
         except KeyError:
             raise SuspiciousOperation("Could not find machine serial number")
-        if not serial_number:
+        if not self.serial_number:
             raise SuspiciousOperation("Empty serial number")
-        return serial_number, event
+
+    def enroll_machine(self):
+        try:
+            request = verify_enrollment_secret(
+                "jamf_protect_enrollment",
+                self.token, self.user_agent, self.ip,
+                serial_number=self.serial_number
+            )
+        except EnrollmentSecretVerificationFailed:
+            raise PermissionDenied("Invalid enrollment secret")
+        else:
+            # get or create enrolled machine
+            enrollment_secret = request.enrollment_secret
+            self.enrolled_machine, _ = EnrolledMachine.objects.get_or_create(
+                enrollment=enrollment_secret.jamf_protect_enrollment,
+                serial_number=self.serial_number
+            )
+
+            # apply enrollment secret tags
+            for tag in enrollment_secret.tags.all():
+                MachineTag.objects.get_or_create(serial_number=self.serial_number, tag=tag)
+            post_enrollment_event(self.serial_number, self.user_agent, self.ip, {'action': 'enrollment'})
+
+    def commit_machine_snapshot(self, max_age=3600):
+        module = 'zentral.contrib.jamf_protect'
+        if MetaMachine(self.serial_number).has_recent_source_snapshot(module, max_age):
+            logger.debug("Skip Jamf Protect machine snapshot commit for machine %s.", self.serial_number)
+            return
+        tree = {'source': {'module': module,
+                           'name': 'Jamf Protect'},
+                'reference': self.serial_number,
+                'serial_number': self.serial_number,
+                'public_ip_address': self.ip}
+        hostname = self.event.get("host", {}).get("hostname")
+        if hostname:
+            tree['system_info'] = {'computer_name': hostname}
+        business_unit = self.enrolled_machine.enrollment.secret.get_api_enrollment_business_unit()
+        if business_unit:
+            tree['business_unit'] = business_unit.serialize()
+        commit_machine_snapshot_and_trigger_events(tree)
 
     def post(self, request, *args, **kwargs):
-        token = self.get_token(request)
-        user_agent, ip = user_agent_and_ip_address_from_request(request)
-        serial_number, event = self.load_event(request)
-        if not EnrolledMachine.objects.filter(enrollment__secret__secret=token,
-                                              serial_number=serial_number).count():
-            try:
-                request = verify_enrollment_secret(
-                    "jamf_protect_enrollment", token, user_agent, ip,
-                    serial_number=serial_number
-                )
-            except EnrollmentSecretVerificationFailed:
-                raise PermissionDenied("Invalid enrollment secret")
-            else:
-                # get or create enrolled machine
-                enrollment_secret = request.enrollment_secret
-                enrolled_machine, _ = EnrolledMachine.objects.get_or_create(
-                    enrollment=enrollment_secret.jamf_protect_enrollment,
-                    serial_number=serial_number
-                )
-
-                # apply enrollment secret tags
-                for tag in enrollment_secret.tags.all():
-                    MachineTag.objects.get_or_create(serial_number=serial_number, tag=tag)
-                post_enrollment_event(serial_number, user_agent, ip, {'action': 'enrollment'})
-        post_event(serial_number, user_agent, ip, event)
+        self.user_agent, self.ip = user_agent_and_ip_address_from_request(request)
+        self.load_token(request)
+        self.load_event(request)
+        try:
+            self.enrolled_machine = EnrolledMachine.objects.select_related(
+                "enrollment__secret__meta_business_unit"
+            ).get(
+                enrollment__secret__secret=self.token,
+                serial_number=self.serial_number
+            )
+        except EnrolledMachine.DoesNotExist:
+            self.enroll_machine()
+        self.commit_machine_snapshot()
+        post_event(self.serial_number, self.user_agent, self.ip, self.event)
         return HttpResponse("OK")
