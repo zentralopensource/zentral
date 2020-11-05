0329f2892c67c8f3def1285ad83c69e268c2b8ad
Éric Falconnier <eric.falconnier@112hz.com>
2020-11-05 21:20:06 +0100

    Prepare docker entrypoint
    Only use django db migration to wait for DB
    Auto worker number for gunicorn

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import multiprocessing
 import os
 import random
-import socket
 import subprocess
 import sys
 import time
@@ -13,42 +13,18 @@ except NotImplementedError:
     warnings.warn('No secure pseudo random number generator available.')
 
 
-def check_db_connection(connection):
-    try:
-        socket.create_connection(connection).close()
-    except socket.error:
-        return False
-    else:
-        return True
-
-
-def wait_for_db():
-    connection = (os.environ.get('POSTGRES_HOST', 'localhost'),
-                  os.environ.get('POSTGRES_PORT', '5432'))
-    while not check_db_connection(connection):
-        reconnect_delay = 1000 / random.randint(500, 3000)
-        warnings.warn("Can't connect to DB! Sleep {:.2f}s…".format(reconnect_delay))
-        time.sleep(reconnect_delay)
-    print("DB connection OK")
-
-
-def django_migrate_db():
-    try:
-        subprocess.check_call(['python', 'server/manage.py', 'migrate', '--noinput'])
-    except subprocess.CalledProcessError:
-        return False
-    else:
-        return True
-
-
 def wait_for_db_migration():
-    wait_for_db()
     i = 0
-    while not django_migrate_db():
-        retry_delay = (i + 1) * random.uniform(0.8, 1.2)
-        warnings.warn("Can't migrate DB! Sleep {:.2f}s…".format(retry_delay))
-        time.sleep(retry_delay)
-        i += 1
+    while True:
+        try:
+            subprocess.check_call(['python', 'server/manage.py', 'migrate', '--noinput'])
+        except subprocess.CalledProcessError:
+            retry_delay = min(20, (i + 1)) * random.uniform(0.8, 1.2)
+            warnings.warn("Can't migrate DB! Sleep {:.1f}s…".format(retry_delay))
+            time.sleep(retry_delay)
+            i += 1
+        else:
+            break
     print("DB migration OK")
 
 
@@ -60,7 +36,7 @@ KNOWN_COMMANDS = {
     "runserver": ["python", 'server/manage.py', 'runserver', '0.0.0.0:8000'],
     "gunicorn": ["gunicorn", "--chdir", "/zentral/server",
                              "-b", "0.0.0.0:8000",
-                             "-w", "4",
+                             "-w", str(2 * multiprocessing.cpu_count() + 1),
                              "--access-logfile", "-",
                              "--error-logfile", "-",
                              "server.wsgi"],
