caf62129781f0ceec4b1b0dfb51b39a905a092f6
Éric Falconnier <eric.falconnier@112hz.com>
2020-11-23 18:51:20 +0100

    Better solution for custom error pages
    Add build_custom_error_pages management command to render correct html
    error pages that can be used by the HTTP proxy, when the application is
    failing.
    Remove buggy nginx error_page docker configurations. The new pages are
    not configured, because they need to be built before they can be used.
    It could be done in the docker-entrypoint, but the web app start is
    already slow enough. PRs welcomed!

---
+++ server/base/management/commands/build_custom_error_pages.py
@@ -0,0 +1,31 @@
+import logging
+import os
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.template.loader import get_template
+
+
+logger = logging.getLogger("zentral.server.base.management.commands.build_custom_error_pages")
+
+
+class Command(BaseCommand):
+    help = 'Build custom error pages'
+    errors = (
+      (404, "Not Found"),
+      (500, "Internal Server Error"),
+      (502, "Bad Gateway"),
+      (503, "Service Unavailable"),
+      (504, "Gateway Timeout"),
+    )
+
+    def handle(self, *args, **options):
+        template = get_template("custom_error_page.html")
+        basedir = os.path.join(settings.STATIC_ROOT, "custom_error_pages")
+        os.makedirs(basedir, exist_ok=True)
+        for status_code, message in self.errors:
+            page_content = template.render({
+                "status_code": status_code,
+                "message": message,
+            })
+            with open(os.path.join(basedir, f"{status_code}.html"), "w") as f:
+                f.write(page_content)
