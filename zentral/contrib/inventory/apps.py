9c7e33c732d571e91ddd7f6980995fa020b02bf6
Éric Falconnier <eric.falconnier@112hz.com>
2021-03-15 19:32:13 +0100

    Add file permissions for santa fileinfo ingestion

--- zentral/contrib/inventory/apps.py
+++ zentral/contrib/inventory/apps.py
@@ -4,3 +4,4 @@ from zentral.utils.apps import ZentralAppConfig
 class ZentralInventoryAppConfig(ZentralAppConfig):
     name = "zentral.contrib.inventory"
     verbose_name = "Zentral inventory contrib app"
+    permission_models = ("file",)
