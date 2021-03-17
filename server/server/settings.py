54ddb21b9ca0e9f5c041e1cfa088f8a08f8abe1a
Éric Falconnier <eric.falconnier@112hz.com>
2021-03-17 16:26:48 +0100

    Disable DRF browsable renderer

--- server/server/settings.py
+++ server/server/settings.py
@@ -83,6 +83,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    # disable the Browsable API Renderer
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
