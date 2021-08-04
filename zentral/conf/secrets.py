56b879e7ba8c54ea3112efc14e10bcd47b90e375
Éric Falconnier <eric.falconnier@112hz.com>
2021-08-04 16:10:39 +0200

    Fix log level of the config secret plugins
    Use debug instead of info

--- zentral/conf/secrets.py
+++ zentral/conf/secrets.py
@@ -14,7 +14,7 @@ class AWSSecretClient:
         self._client = boto3.client('secretsmanager')
 
     def get(self, name):
-        logger.info("Get AWS secret %s", name)
+        logger.debug("Get AWS secret %s", name)
         return self._client.get_secret_value(SecretId=name)
 
 
@@ -32,7 +32,7 @@ class GCPSecretClient:
             raise RuntimeError("Could not get default GCP project")
 
     def get(self, name):
-        logger.info("Get GCP secret %s", name)
+        logger.debug("Get GCP secret %s", name)
         path = "{}/versions/{}".format(self._client.secret_path(self._project, name), "latest")
         return self._client.access_secret_version(request={"name": path}).payload.data.decode("UTF-8")
 
@@ -42,9 +42,9 @@ def get_secret_client():
         try:
             client = client_class()
         except Exception as e:
-            logger.info("Cloud not instantiate %s secret client: %s",
-                        client_class.platform, e)
+            logger.debug("Cloud not instantiate %s secret client: %s",
+                         client_class.platform, e)
         else:
-            logger.info("%s secret client instantiated", client.platform)
+            logger.debug("%s secret client instantiated", client.platform)
             return client
     raise RuntimeError("Could not instantiate secret client")
