b0b057c9a6dc9bc798704bb0177ebc2543303d43
Éric Falconnier <eric.falconnier@112hz.com>
2021-09-08 12:09:15 +0200

    Fix zentral configuration AWS secret resolver

--- zentral/conf/secrets.py
+++ zentral/conf/secrets.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 
 
 logger = logging.getLogger("zentral.conf.secrets")
@@ -6,16 +7,34 @@ logger = logging.getLogger("zentral.conf.secrets")
 
 class AWSSecretClient:
     platform = "AWS"
+    metadata_service_url = "http://169.254.169.254/latest/"
 
     def __init__(self):
         import boto3
         sts = boto3.client("sts")
+        # fail early if no authentication
         sts.get_caller_identity()
-        self._client = boto3.client('secretsmanager')
+        # get region
+        r = requests.put(
+            self.metadata_service_url + "api/token",
+            headers={"X-aws-ec2-metadata-token-ttl-seconds": "10"},
+            timeout=.5
+        )
+        r.raise_for_status()
+        token = r.text.strip()
+        r = requests.get(
+            self.metadata_service_url + "dynamic/instance-identity/document",
+            headers={"X-aws-ec2-metadata-token": token}
+        )
+        r.raise_for_status()
+        data = r.json()
+        region = data["region"]
+        # setup secrets manager client
+        self._client = boto3.client('secretsmanager', region_name=region)
 
     def get(self, name):
         logger.debug("Get AWS secret %s", name)
-        return self._client.get_secret_value(SecretId=name)
+        return self._client.get_secret_value(SecretId=name)["SecretString"]
 
 
 class GCPSecretClient:
