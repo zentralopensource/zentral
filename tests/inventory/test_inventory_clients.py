e157a79173104e8225fe57638db92abc2a5c6f0c
Éric Falconnier <eric.falconnier@112hz.com>
2021-12-02 21:19:30 +0100

    Fix flaky inventory client workers test

--- tests/inventory/test_inventory_clients.py
+++ tests/inventory/test_inventory_clients.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from zentral.conf import settings
 from zentral.contrib.inventory.workers import get_workers
 
 
@@ -8,4 +9,7 @@ class InventoryClientsTestCase(SimpleTestCase):
         for worker in get_workers():
             worker_count += 1
             self.assertTrue(isinstance(worker.client.source["config"], dict))
-        self.assertTrue(worker_count > 0)
+        self.assertEqual(
+            worker_count,
+            len(settings.get('apps', {}).get('zentral.contrib.inventory', {}).get('clients', []))
+        )
