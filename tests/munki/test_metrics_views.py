f053203a3ee78f4b4b3770a50cc7c3c065772084
Éric Falconnier <eric.falconnier@112hz.com>
2021-12-08 18:52:26 +0100

    Add buckets for munki installed pkginfos metrics

--- tests/munki/test_metrics_views.py
+++ tests/munki/test_metrics_views.py
@@ -1,17 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.urls import reverse
 from django.test import TestCase
 from django.utils.crypto import get_random_string
 from prometheus_client.parser import text_string_to_metric_families
-from zentral.contrib.munki.models import ManagedInstall
+from zentral.contrib.munki.models import ManagedInstall, MunkiState
 from zentral.conf import settings
 
 
 class MunkiMetricsViewsTestCase(TestCase):
     # utility methods
 
-    def _force_managed_install(self, failed=False, count=1, reinstall=False):
-        return ManagedInstall.objects.create(
+    def _force_managed_install(self, failed=False, count=1, reinstall=False, age_days=22):
+        mi = ManagedInstall.objects.create(
             machine_serial_number=get_random_string(),
             name=get_random_string(),
             installed_version=get_random_string(),
@@ -20,6 +20,10 @@ class MunkiMetricsViewsTestCase(TestCase):
             failed_at=datetime.now() if failed else None,
             reinstall=reinstall,
         )
+        last_seen = datetime.utcnow() - timedelta(days=age_days)
+        ms = MunkiState.objects.create(machine_serial_number=mi.machine_serial_number)
+        MunkiState.objects.filter(pk=ms.pk).update(last_seen=last_seen)
+        return mi
 
     def _make_authenticated_request(self):
         return self.client.get(reverse("munki_metrics:all"),
@@ -36,18 +40,21 @@ class MunkiMetricsViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_installed_pkginfos(self):
-        mi = self._force_managed_install()
+        mi = self._force_managed_install(age_days=22)
         response = self._make_authenticated_request()
         for family in text_string_to_metric_families(response.content.decode("utf-8")):
-            if family.name != "zentral_munki_installed_pkginfos":
+            if family.name != "zentral_munki_installed_pkginfos_bucket":
                 continue
             else:
-                self.assertEqual(len(family.samples), 1)
-                sample = family.samples[0]
-                self.assertEqual(sample.value, 1)
-                self.assertEqual(sample.labels["name"], mi.name)
-                self.assertEqual(sample.labels["version"], mi.installed_version)
-                self.assertEqual(sample.labels["reinstall"], "no" if not mi.reinstall else "yes")
+                self.assertEqual(len(family.samples), 7)
+                for sample in family.samples:
+                    self.assertEqual(sample.labels["name"], mi.name)
+                    self.assertEqual(sample.labels["version"], mi.installed_version)
+                    if sample.labels["le"] in ("1", "7", "14"):
+                        # last seen 22 days ago
+                        self.assertEqual(sample.value, 0)
+                    else:
+                        self.assertEqual(sample.value, 1)
                 break
         else:
             raise AssertionError("could not find expected metric family")
--- zentral/contrib/munki/metrics_views.py
+++ zentral/contrib/munki/metrics_views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.db.models import Count
 from prometheus_client import Gauge
 from zentral.utils.prometheus import BasePrometheusMetricsView
@@ -5,21 +6,53 @@ from .models import ManagedInstall
 
 
 class MetricsView(BasePrometheusMetricsView):
-    def populate_registry(self):
-        g = Gauge('zentral_munki_installed_pkginfos', 'Zentral Munki installed pkginfos',
-                  ['name', 'version', 'reinstall'],
+    def add_installed_pkginfos_buckets(self):
+        query = (
+            "with active_machines as ("
+            "  select machine_serial_number, date_part('days', now() - last_seen) as age"
+            "  from munki_munkistate"
+            "), installed_versions as ("
+            "  select mi.name, mi.installed_version as version,"
+            '  case when am.age <= 1 then 1 else 0 end as "1",'
+            '  case when am.age <= 7 then 1 else 0 end as "7",'
+            '  case when am.age <= 14 then 1 else 0 end as "14",'
+            '  case when am.age <= 30 then 1 else 0 end as "30",'
+            '  case when am.age <= 45 then 1 else 0 end as "45",'
+            '  case when am.age <= 90 then 1 else 0 end as "90"'
+            "  from munki_managedinstall as mi"
+            "  join active_machines as am on (am.machine_serial_number = mi.machine_serial_number)"
+            "  where mi.installed_version is not null"
+            ") "
+            "select name, version,"
+            'sum("1") as "1",'
+            'sum("7") as "7",'
+            'sum("14") as "14",'
+            'sum("30") as "30",'
+            'sum("45") as "45",'
+            'sum("90") as "90",'
+            'sum(1) as "+Inf" '
+            "from installed_versions "
+            "group by name, version"
+        )
+        cursor = connection.cursor()
+        cursor.execute(query)
+        columns = [c.name for c in cursor.description]
+
+        g = Gauge('zentral_munki_installed_pkginfos_bucket', 'Zentral Munki installed pkginfos',
+                  ['name', 'version', 'le'],
                   registry=self.registry)
-        for managed_install in (ManagedInstall.objects
-                                              .filter(installed_version__isnull=False)
-                                              .values("name", "installed_version", "reinstall")
-                                              .annotate(count=Count("machine_serial_number"))):
-            g.labels(
-                name=managed_install["name"],
-                version=managed_install["installed_version"],
-                reinstall="yes" if managed_install["reinstall"] else "no",
-            ).set(
-                managed_install["count"]
-            )
+        for row in cursor.fetchall():
+            row_d = dict(zip(columns, row))
+            for le in ("1", "7", "14", "30", "45", "90", "+Inf"):
+                g.labels(
+                    name=row_d["name"],
+                    version=row_d["version"],
+                    le=le
+                ).set(
+                    row_d[le]
+                )
+
+    def add_failed_pkginfos(self):
         g = Gauge('zentral_munki_failed_pkginfos', 'Zentral Munki failed pkginfos',
                   ['name', 'version'],
                   registry=self.registry)
@@ -33,3 +66,7 @@ class MetricsView(BasePrometheusMetricsView):
             ).set(
                 managed_install["count"]
             )
+
+    def populate_registry(self):
+        self.add_installed_pkginfos_buckets()
+        self.add_failed_pkginfos()
