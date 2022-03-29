c12d8e989d4e5b7b24230c1a14f19e6723944866
Éric Falconnier <eric.falconnier@112hz.com>
2022-03-29 08:58:44 +0200

    Add missing apps to test config

---
+++ tests/server_base/test_apps.py
@@ -0,0 +1,41 @@
+from django.test import SimpleTestCase
+from django.apps import apps
+from zentral.utils.apps import ZentralAppConfig
+
+
+class TestZentralApps(SimpleTestCase):
+    maxDiff = None
+    expected_apps = [
+        "accounts",
+        "compliance_checks",
+        "filebeat",
+        "incidents",
+        "inventory",
+        "jamf",
+        "jamf_protect",
+        "mdm",
+        "monolith",
+        "munki",
+        "nagios",
+        "probes",
+        "puppet",
+        "okta",
+        "osquery",
+        "realms",
+        "santa",
+        "simplemdm",
+        "wsone",
+        "xnumon",
+        "zendesk",
+    ]
+
+    def test_zentral_apps(self):
+        found_apps = []
+        ok_apps = []
+        for name, app_config in apps.app_configs.items():
+            if name in self.expected_apps:
+                found_apps.append(name)
+                if isinstance(app_config, ZentralAppConfig):
+                    ok_apps.append(name)
+        self.assertEqual(found_apps, ok_apps)
+        self.assertEqual(sorted(found_apps), sorted(self.expected_apps))
