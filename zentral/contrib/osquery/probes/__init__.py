8321c95295c30bac5bf0939d69c369f20cf15510
Éric Falconnier <eric.falconnier@112hz.com>
2022-05-25 07:30:21 +0200

    Remove legacy osquery probe modules
    Those modules were kept for over a year to migrate the legacy probes to
    the new models. They are not necessary anymore.

--- zentral/contrib/osquery/probes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import OsqueryProbe  # NOQA
-from .osquery_compliance import OsqueryComplianceProbe  # NOQA
-from .osquery_distributed_query import OsqueryDistributedQueryProbe  # NOQA
-from .osquery_file_carve import OsqueryFileCarveProbe  # NOQA
-from .osquery_fim import OsqueryFIMProbe  # NOQA
