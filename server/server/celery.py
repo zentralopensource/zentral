f780f650330a28157c31481c447678f566758798
Éric Falconnier <eric.falconnier@112hz.com>
2022-07-18 11:42:16 +0200

    Upgrade django-celery-results to v2.4.0
    To address CVE-2020-17495, we use django-celery-results v2.4.0 and
    configure explicitly the storage of the tasks arguments.

--- server/server/celery.py
+++ server/server/celery.py
@@ -3,6 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 app = Celery('zentral')
+app.conf.result_extended = True
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
