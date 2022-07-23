c8962641ad78ec388690a9743f4b18bc8f6a2131
Éric Falconnier <eric.falconnier@112hz.com>
2022-07-23 19:48:39 +0200

    Add AWS auth to OpenSearch store backend

--- zentral/core/stores/backends/opensearch.py
+++ zentral/core/stores/backends/opensearch.py
@@ -1,7 +1,9 @@
 import logging
-from opensearchpy import OpenSearch
+import boto3
+from opensearchpy import AWSV4SignerAuth, OpenSearch, RequestsHttpConnection
 from opensearchpy.helpers import streaming_bulk
 from opensearchpy.exceptions import ConnectionError, RequestError
+from zentral.core.exceptions import ImproperlyConfigured
 from .es_os_base import ESOSEventStore
 
 
@@ -12,6 +14,30 @@ class EventStore(ESOSEventStore):
     client_class = OpenSearch
     connection_error_class = ConnectionError
     request_error_class = RequestError
+
+    def _get_client_kwargs(self, config_d):
+        kwargs = super()._get_client_kwargs(config_d)
+        aws_auth = config_d.get("aws_auth")
+        if aws_auth is None:
+            return
+        try:
+            region = aws_auth["region"]
+        except KeyError:
+            raise ImproperlyConfigured("Missing region in aws_auth")
+        try:
+            access_key_id, secret_access_key = aws_auth["access_key_id"], aws_auth["secret_access_key"]
+        except KeyError:
+            credentials = boto3.Session().get_credentials()
+        else:
+            credentials = boto3.Session(
+                aws_access_key_id=access_key_id,
+                aws_secret_access_key=secret_access_key
+            ).get_credentials()
+        if not credentials:
+            raise ImproperlyConfigured("Could not get AWS credentials")
+        kwargs["http_auth"] = AWSV4SignerAuth(credentials, region)
+        kwargs["connection_class"] = RequestsHttpConnection
+        return kwargs
 
     def _streaming_bulk(self, *args, **kwargs):
         return streaming_bulk(*args, **kwargs)
