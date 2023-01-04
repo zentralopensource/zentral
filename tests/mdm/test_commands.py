9c2876f5b137b0f518ace0393f4a72627bd3509c
Éric Falconnier <eric.falconnier@112hz.com>
2023-01-04 17:56:56 +0100

    Better MDM RemoveProfile command tests

--- tests/mdm/test_commands.py
+++ tests/mdm/test_commands.py
@@ -1,65 +1,18 @@
-import copy
-import plistlib
 import uuid
 from datetime import datetime
-from django.http import HttpResponse
 from django.test import TestCase
 from django.utils.crypto import get_random_string
 from zentral.contrib.inventory.models import EnrollmentSecret, MetaBusinessUnit
 from zentral.contrib.mdm.declarations import update_blueprint_activation, update_blueprint_declaration_items
-from zentral.contrib.mdm.models import (Artifact, ArtifactType, ArtifactVersion,
-                                        Blueprint, BlueprintArtifact,
+from zentral.contrib.mdm.models import (Blueprint,
                                         Channel, CommandStatus, RequestStatus,
-                                        DeviceArtifact,
                                         DEPEnrollmentSession,
                                         EnrolledDevice, EnrolledUser,
-                                        Platform, Profile, PushCertificate,
-                                        UserArtifact)
-from zentral.contrib.mdm.commands import DeviceInformation, RemoveProfile
+                                        PushCertificate)
+from zentral.contrib.mdm.commands import DeviceInformation
 from zentral.contrib.mdm.commands.utils import (_get_next_queued_command,
-                                                _remove_artifacts,
                                                 _update_inventory)
 from .utils import force_dep_enrollment, force_realm_user
-
-
-PROFILE_TEMPLATE = {
-    'PayloadContent': [{
-        'PayloadType': 'com.apple.dock',
-        'PayloadDescription': 'Dock Payload',
-        'PayloadDisplayName': 'Dock',
-        'PayloadVersion': 1,
-        'orientation': 'right'
-    }],
-    'PayloadType': 'Configuration',
-    'PayloadDescription': 'Superbe profile imbattable!!!',
-    'PayloadDisplayName': 'Test User Profile with Dock',
-    'PayloadVersion': 1,
-    'PayloadOrganization': 'Zentral',
-    'PayloadScope': 'User',
-}
-
-
-def build_profile(
-    payload_display_name=None,
-    payload_description=None,
-    payload_identifier=None,
-    payload_uuid=None,
-    channel=Channel.Device
-):
-    if payload_uuid is None:
-        payload_uuid = str(uuid.uuid4()).upper()
-    if payload_identifier is None:
-        payload_identifier = f"io.zentral.test.{payload_uuid}"
-    profile = copy.deepcopy(PROFILE_TEMPLATE)
-    profile["PayloadIdentifier"] = payload_identifier
-    profile["PayloadUUID"] = payload_uuid
-    profile["PayloadDisplayName"] = payload_display_name or get_random_string(16)
-    profile["PayloadDescription"] = payload_description or get_random_string(32)
-    profile["PayloadScope"] = "System" if channel == Channel.Device else "User"
-    payload = profile["PayloadContent"][0]
-    payload["PayloadIdentifier"] = f"{payload_identifier}.0"
-    payload["PayloadUUID"] = str(uuid.uuid4()).upper()
-    return plistlib.dumps(profile)
 
 
 class TestMDMCommands(TestCase):
@@ -136,94 +89,6 @@ class TestMDMCommands(TestCase):
         cls.dep_enrollment_session.set_authenticated_status(cls.enrolled_device)
         cls.dep_enrollment_session.set_completed_status(cls.enrolled_device)
 
-    def _force_artifact(
-        self,
-        version_count=1,
-        artifact_type=ArtifactType.Profile,
-        channel=Channel.Device,
-        platforms=None,
-        install_before_setup_assistant=False,
-        auto_update=True,
-        priority=0
-    ):
-        if platforms is None:
-            platforms = Platform.all_values()
-        artifact = Artifact.objects.create(
-            name=get_random_string(32),
-            type=artifact_type.name,
-            channel=channel.name,
-            platforms=platforms
-        )
-        artifact_versions = []
-        payload_identifier = "{}.{}.{}".format(get_random_string(2), get_random_string(4), str(uuid.uuid4()))
-        payload_identifier = None
-        for version in range(version_count, 0, -1):
-            artifact_version = ArtifactVersion.objects.create(artifact=artifact, version=version)
-            artifact_versions.append(artifact_version)
-            if artifact_type == ArtifactType.Profile:
-                if payload_identifier is None:
-                    payload_identifier = "{}.{}.{}".format(get_random_string(2),
-                                                           get_random_string(4),
-                                                           str(uuid.uuid4()).upper())
-                payload_uuid = str(uuid.uuid4()).upper()
-                payload_display_name = get_random_string(16)
-                payload_description = get_random_string(32)
-                Profile.objects.create(
-                    artifact_version=artifact_version,
-                    source=build_profile(
-                        payload_display_name=payload_display_name,
-                        payload_description=payload_description,
-                        payload_identifier=payload_identifier,
-                        payload_uuid=payload_uuid,
-                        channel=channel
-                    ),
-                    payload_identifier=payload_identifier,
-                    payload_uuid=payload_uuid,
-                    payload_display_name=payload_display_name,
-                    payload_description=payload_description
-                )
-        return artifact, artifact_versions
-
-    def _force_blueprint_artifact(
-        self,
-        version_count=1,
-        artifact_type=ArtifactType.Profile,
-        channel=Channel.Device,
-        platforms=None,
-        install_before_setup_assistant=False,
-        auto_update=True,
-        priority=0,
-        blueprint=None
-    ):
-        artifact, artifact_versions = self._force_artifact(
-            version_count,
-            artifact_type,
-            channel,
-            platforms,
-            install_before_setup_assistant,
-            auto_update,
-            priority
-        )
-        BlueprintArtifact.objects.create(
-            blueprint=blueprint or self.blueprint1,
-            artifact=artifact,
-            install_before_setup_assistant=install_before_setup_assistant,
-            auto_update=auto_update,
-            priority=priority,
-        )
-        return artifact, artifact_versions
-
-    def _force_target_artifact_version(self, target, artifact_version):
-        kwargs = {"artifact_version__artifact": artifact_version.artifact,
-                  "defaults": {"artifact_version": artifact_version}}
-        if isinstance(target, EnrolledDevice):
-            model = DeviceArtifact
-            kwargs["enrolled_device"] = target
-        else:
-            model = UserArtifact
-            kwargs["enrolled_user"] = target
-        return model.objects.update_or_create(**kwargs)[0]
-
     # _get_next_queued_command
 
     def test_no_next_queues_command(self):
@@ -279,134 +144,6 @@ class TestMDMCommands(TestCase):
             None,
         )
         self.assertEqual(cmd, cmd2)
-
-    # _remove_artifacts
-
-    def test_remove_device_profile_noop(self):
-        artifact, artifact_versions = self._force_artifact()
-        self.assertIsNone(_remove_artifacts(
-            Channel.Device, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        ))
-
-    def test_remove_device_profile_notnow_noop(self):
-        artifact, artifact_versions = self._force_artifact()
-        self._force_target_artifact_version(self.enrolled_device, artifact_versions[0])
-        self.assertIsNone(_remove_artifacts(
-            Channel.Device, RequestStatus.NotNow,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        ))
-
-    def test_remove_device_profile(self):
-        artifact, artifact_versions = self._force_artifact()
-        device_artifact = self._force_target_artifact_version(self.enrolled_device, artifact_versions[0])
-        command = _remove_artifacts(
-            Channel.Device, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        )
-        self.assertIsInstance(command, RemoveProfile)
-        self.assertEqual(command.channel, Channel.Device)
-        self.assertEqual(command.db_command.artifact_version, artifact_versions[0])
-        http_response = command.build_http_response(self.dep_enrollment_session)
-        self.assertIsInstance(http_response, HttpResponse)
-        self.assertIsNone(_remove_artifacts(
-            Channel.User, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        ))
-        qs = DeviceArtifact.objects.filter(enrolled_device=self.enrolled_device)
-        self.assertEqual(qs.count(), 1)
-        self.assertEqual(qs.first(), device_artifact)
-        command.process_response({"Status": "Acknowledged"}, self.dep_enrollment_session, self.meta_business_unit)
-        self.assertEqual(qs.count(), 0)
-
-    def test_no_remove_device_profile_previous_error(self):
-        artifact, artifact_versions = self._force_artifact()
-        self._force_target_artifact_version(self.enrolled_device, artifact_versions[0])
-        command = _remove_artifacts(
-            Channel.Device, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        )
-        command.process_response({"Status": "Error", "ErrorChain": [{"un": 1}]},
-                                 self.dep_enrollment_session, self.meta_business_unit)
-        self.assertIsNone(_remove_artifacts(
-            Channel.Device, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        ))
-
-    def test_remove_user_profile_noop(self):
-        artifact, artifact_versions = self._force_artifact()
-        self.assertIsNone(_remove_artifacts(
-            Channel.User, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        ))
-
-    def test_remove_user_profile_notnow_noop(self):
-        artifact, artifact_versions = self._force_artifact()
-        self._force_target_artifact_version(self.enrolled_user, artifact_versions[0])
-        self.assertIsNone(_remove_artifacts(
-            Channel.User, RequestStatus.NotNow,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        ))
-
-    def test_remove_user_profile(self):
-        artifact, artifact_versions = self._force_artifact()
-        user_artifact = self._force_target_artifact_version(self.enrolled_user, artifact_versions[0])
-        command = _remove_artifacts(
-            Channel.User, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        )
-        self.assertIsInstance(command, RemoveProfile)
-        self.assertEqual(command.channel, Channel.User)
-        self.assertEqual(command.db_command.artifact_version, artifact_versions[0])
-        http_response = command.build_http_response(self.dep_enrollment_session)
-        self.assertIsInstance(http_response, HttpResponse)
-        self.assertIsNone(_remove_artifacts(
-            Channel.Device, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            None
-        ))
-        qs = UserArtifact.objects.filter(enrolled_user=self.enrolled_user)
-        self.assertEqual(qs.count(), 1)
-        self.assertEqual(qs.first(), user_artifact)
-        command.process_response({"Status": "Acknowledged"}, self.dep_enrollment_session, self.meta_business_unit)
-        self.assertEqual(qs.count(), 0)
-
-    def test_no_remove_user_profile_previous_error(self):
-        artifact, artifact_versions = self._force_artifact()
-        self._force_target_artifact_version(self.enrolled_user, artifact_versions[0])
-        command = _remove_artifacts(
-            Channel.User, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        )
-        command.process_response({"Status": "Error", "ErrorChain": [{"un": 1}]},
-                                 self.dep_enrollment_session, self.meta_business_unit)
-        self.assertIsNone(_remove_artifacts(
-            Channel.User, RequestStatus.Idle,
-            self.dep_enrollment_session,
-            self.enrolled_device,
-            self.enrolled_user
-        ))
 
     # update inventory
 
---
+++ tests/mdm/test_remove_profile_command.py
@@ -0,0 +1,324 @@
+import plistlib
+import uuid
+from django.test import TestCase
+from django.utils.crypto import get_random_string
+from zentral.contrib.inventory.models import MetaBusinessUnit
+from zentral.contrib.mdm.commands import RemoveProfile
+from zentral.contrib.mdm.commands.utils import _remove_artifacts
+from zentral.contrib.mdm.models import (
+    Artifact,
+    ArtifactType,
+    ArtifactVersion,
+    Channel,
+    Blueprint,
+    BlueprintArtifact,
+    DeviceArtifact,
+    EnrolledUser,
+    Platform,
+    Profile,
+    RequestStatus,
+    TargetArtifactStatus,
+    UserArtifact,
+)
+from .utils import force_dep_enrollment_session
+
+
+class RemoveProfileCommandTestCase(TestCase):
+    maxDiff = None
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.mbu = MetaBusinessUnit.objects.create(name=get_random_string(12))
+        cls.mbu.create_enrollment_business_unit()
+        cls.dep_enrollment_session, _, _ = force_dep_enrollment_session(
+            cls.mbu, authenticated=True, completed=True, realm_user=True
+        )
+        cls.enrolled_device = cls.dep_enrollment_session.enrolled_device
+        cls.blueprint = Blueprint.objects.create(name=get_random_string(12))
+        cls.enrolled_device.blueprint = cls.blueprint
+        cls.enrolled_device.save()
+        cls.enrolled_user = EnrolledUser.objects.create(
+            enrolled_device=cls.enrolled_device,
+            user_id=str(uuid.uuid4()).upper(),
+            long_name=get_random_string(12),
+            short_name=get_random_string(12),
+            token=get_random_string(12).encode("utf-8"),
+        )
+
+    def _force_profile(
+        self,
+        channel=Channel.Device,
+        artifact=None,
+        version=None,
+        payload_content=None,
+        status=None,
+        in_blueprint=False,
+    ):
+        if artifact is None:
+            artifact_name = get_random_string(12)
+            artifact = Artifact.objects.create(
+                name=artifact_name,
+                type=ArtifactType.Profile.name,
+                channel=channel.name,
+                platforms=[Platform.macOS.name],
+            )
+        else:
+            artifact_name = artifact.name
+        artifact_version = ArtifactVersion.objects.create(
+            artifact=artifact, version=version or 0
+        )
+        try:
+            payload_identifier = (
+                artifact.artifactversion_set.first().profile.payload_identifier
+            )
+        except Exception:
+            payload_identifier = str(uuid.uuid4())
+        profile = Profile.objects.create(
+            artifact_version=artifact_version,
+            filename=f"{artifact_name}.mobileconfig",
+            source=plistlib.dumps(
+                {
+                    "PayloadContent": payload_content if payload_content else [],
+                    "PayloadDisplayName": artifact_name,
+                    "PayloadIdentifier": payload_identifier,
+                    "PayloadRemovalDisallowed": False,
+                    "PayloadType": "Configuration",
+                    "PayloadUUID": str(uuid.uuid4()),
+                    "PayloadVersion": 1,
+                }
+            ),
+            payload_identifier=payload_identifier,
+            payload_display_name=artifact_name,
+            payload_description="",
+        )
+        if status:
+            if channel == Channel.Device:
+                DeviceArtifact.objects.create(
+                    enrolled_device=self.enrolled_device,
+                    artifact_version=artifact_version,
+                    status=status.name,
+                )
+            else:
+                UserArtifact.objects.create(
+                    enrolled_user=self.enrolled_user,
+                    artifact_version=artifact_version,
+                    status=status.name,
+                )
+        if in_blueprint:
+            BlueprintArtifact.objects.create(
+                blueprint=self.blueprint,
+                artifact=artifact,
+                install_before_setup_assistant=False,
+                auto_update=True,
+                priority=100,
+            )
+        return artifact_version, profile
+
+    # verify_channel_and_device
+
+    def test_scope(self):
+        for channel, platform, user_enrollment, result in (
+            (Channel.Device, Platform.iOS, False, True),
+            (Channel.Device, Platform.iPadOS, False, True),
+            (Channel.Device, Platform.macOS, False, True),
+            (Channel.Device, Platform.tvOS, False, True),
+            (Channel.User, Platform.iOS, False, False),
+            (Channel.User, Platform.iPadOS, False, True),
+            (Channel.User, Platform.macOS, False, True),
+            (Channel.User, Platform.tvOS, False, False),
+            (Channel.Device, Platform.iOS, True, True),
+            (Channel.Device, Platform.iPadOS, True, False),
+            (Channel.Device, Platform.macOS, True, True),
+            (Channel.Device, Platform.tvOS, True, False),
+            (Channel.User, Platform.iOS, True, False),
+            (Channel.User, Platform.iPadOS, True, False),
+            (Channel.User, Platform.macOS, True, True),
+            (Channel.User, Platform.tvOS, True, False),
+        ):
+            self.enrolled_device.platform = platform.name
+            self.enrolled_device.user_enrollment = user_enrollment
+            self.assertEqual(
+                result,
+                RemoveProfile.verify_channel_and_device(channel, self.enrolled_device),
+            )
+
+    # build_command
+
+    def test_build_command(self):
+        artifact_version, profile = self._force_profile()
+        cmd = RemoveProfile.create_for_device(self.enrolled_device, artifact_version)
+        response = cmd.build_http_response(self.dep_enrollment_session)
+        payload = plistlib.loads(response.content)["Command"]
+        self.assertEqual(
+            payload,
+            {
+                "RequestType": "RemoveProfile",
+                "Identifier": f"zentral.artifact.{artifact_version.artifact.pk}",
+            },
+        )
+
+    # process_response
+
+    def test_process_acknowledged_response_device(self):
+        artifact_version, _ = self._force_profile(status=TargetArtifactStatus.Installed)
+        qs = DeviceArtifact.objects.filter(
+            enrolled_device=self.enrolled_device,
+            artifact_version__artifact=artifact_version.artifact,
+        )
+        self.assertEqual(qs.count(), 1)
+        self.assertEqual(qs.first().artifact_version, artifact_version)
+        cmd = RemoveProfile.create_for_device(self.enrolled_device, artifact_version)
+        cmd.process_response(
+            {"Status": "Acknowledged"}, self.dep_enrollment_session, self.mbu
+        )
+        self.assertEqual(qs.count(), 0)
+
+    def test_process_acknowledged_response_user(self):
+        artifact_version, _ = self._force_profile(
+            channel=Channel.User, status=TargetArtifactStatus.Installed
+        )
+        qs = UserArtifact.objects.filter(
+            enrolled_user=self.enrolled_user,
+            artifact_version__artifact=artifact_version.artifact,
+        )
+        self.assertEqual(qs.count(), 1)
+        self.assertEqual(qs.first().artifact_version, artifact_version)
+        cmd = RemoveProfile.create_for_user(self.enrolled_user, artifact_version)
+        cmd.process_response(
+            {"Status": "Acknowledged"}, self.dep_enrollment_session, self.mbu
+        )
+        self.assertEqual(qs.count(), 0)
+
+    # _remove_artifacts
+
+    def test_remove_device_profile_noop(self):
+        artifact_version, _ = self._force_profile(
+            status=TargetArtifactStatus.Installed, in_blueprint=True
+        )
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.Device,
+                RequestStatus.Idle,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                None,
+            )
+        )
+
+    def test_remove_device_profile_notnow_noop(self):
+        artifact_version, _ = self._force_profile(status=TargetArtifactStatus.Installed)
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.Device,
+                RequestStatus.NotNow,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                None,
+            )
+        )
+
+    def test_remove_device_profile(self):
+        artifact_version, _ = self._force_profile(status=TargetArtifactStatus.Installed)
+        command = _remove_artifacts(
+            Channel.Device,
+            RequestStatus.Idle,
+            self.dep_enrollment_session,
+            self.enrolled_device,
+            None,
+        )
+        self.assertIsInstance(command, RemoveProfile)
+        self.assertEqual(command.channel, Channel.Device)
+        self.assertEqual(command.artifact_version, artifact_version)
+
+    def test_remove_device_profile_previous_error_noop(self):
+        self._force_profile(status=TargetArtifactStatus.Installed)
+        command = _remove_artifacts(
+            Channel.Device,
+            RequestStatus.Idle,
+            self.dep_enrollment_session,
+            self.enrolled_device,
+            None,
+        )
+        command.process_response(
+            {"Status": "Error", "ErrorChain": [{"un": 1}]},
+            self.dep_enrollment_session,
+            self.mbu,
+        )
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.Device,
+                RequestStatus.Idle,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                None,
+            )
+        )
+
+    def test_remove_user_profile_noop(self):
+        artifact_version, _ = self._force_profile(
+            channel=Channel.User,
+            status=TargetArtifactStatus.Installed,
+            in_blueprint=True,
+        )
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.User,
+                RequestStatus.Idle,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                self.enrolled_user,
+            )
+        )
+
+    def test_remove_user_profile_notnow_noop(self):
+        artifact_version, _ = self._force_profile(
+            channel=Channel.User, status=TargetArtifactStatus.Installed
+        )
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.User,
+                RequestStatus.NotNow,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                self.enrolled_user,
+            )
+        )
+
+    def test_remove_user_profile(self):
+        artifact_version, _ = self._force_profile(
+            channel=Channel.User, status=TargetArtifactStatus.Installed
+        )
+        command = _remove_artifacts(
+            Channel.User,
+            RequestStatus.Idle,
+            self.dep_enrollment_session,
+            self.enrolled_device,
+            self.enrolled_user,
+        )
+        self.assertIsInstance(command, RemoveProfile)
+        self.assertEqual(command.channel, Channel.User)
+        self.assertEqual(command.artifact_version, artifact_version)
+
+    def test_remove_user_profile_previous_error_noop(self):
+        self._force_profile(channel=Channel.User, status=TargetArtifactStatus.Installed)
+        command = _remove_artifacts(
+            Channel.User,
+            RequestStatus.Idle,
+            self.dep_enrollment_session,
+            self.enrolled_device,
+            self.enrolled_user,
+        )
+        command.process_response(
+            {"Status": "Error", "ErrorChain": [{"un": 1}]},
+            self.dep_enrollment_session,
+            self.mbu,
+        )
+        self.assertIsNone(
+            _remove_artifacts(
+                Channel.User,
+                RequestStatus.Idle,
+                self.dep_enrollment_session,
+                self.enrolled_device,
+                self.enrolled_user,
+            )
+        )
