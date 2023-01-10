e79d0b038db1e3acdfd71db2597cbad299eb6dfc
Jason Scurtu <jason@zentral.pro>
2023-01-10 09:42:58 +0100

    implement santa enrollment api

--- zentral/contrib/santa/api_urls.py
+++ zentral/contrib/santa/api_urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .api_views import IngestFileInfo, RuleList, RuleSetUpdate, TargetsExport, ConfigurationList, ConfigurationDetail
+from .api_views import (IngestFileInfo, RuleList, RuleSetUpdate, TargetsExport, ConfigurationList,
+                        ConfigurationDetail, EnrollmentList, EnrollmentDetail,
+                        EnrollmentConfigurationPlist, EnrollmentConfigurationProfile)
 
 
 app_name = "santa_api"
 urlpatterns = [
     path('configurations/', ConfigurationList.as_view(), name="configurations"),
     path('configurations/<int:pk>/', ConfigurationDetail.as_view(), name="configuration"),
+    path('enrollments/', EnrollmentList.as_view(), name="enrollments"),
+    path('enrollments/<int:pk>/', EnrollmentDetail.as_view(), name="enrollment"),
+    path('enrollments/<int:pk>/configuration_plist/', EnrollmentConfigurationPlist.as_view(),
+         name="enrollment_plist"),
+    path('enrollments/<int:pk>/configuration_profile/', EnrollmentConfigurationProfile.as_view(),
+         name="enrollment_profile"),
     path('ingest/fileinfo/', IngestFileInfo.as_view(), name="ingest_file_info"),
     path('rules/', RuleList.as_view(), name="rules"),
     path('rulesets/update/', RuleSetUpdate.as_view(), name="ruleset_update"),
--- zentral/contrib/santa/api_views.py
+++ zentral/contrib/santa/api_views.py
@@ -1,6 +1,9 @@
+import base64
+
 from django.db import transaction
 from django.db.models import F
 from django_filters import rest_framework as filters
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
 from rest_framework import generics, status
@@ -10,11 +13,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_yaml.parsers import YAMLParser
 from zentral.contrib.inventory.models import File, Tag
+from zentral.contrib.santa.utils import build_configuration_plist, build_configuration_profile
 from zentral.utils.drf import DefaultDjangoModelPermissions, DjangoPermissionRequired
 from .events import post_santa_ruleset_update_events
-from .models import Configuration, Rule, RuleSet, Target, translate_rule_policy
+from .models import Configuration, Rule, RuleSet, Target, Enrollment, translate_rule_policy
 from .serializers import (RuleSerializer, RuleSetUpdateSerializer, ConfigurationSerializer,
-                          build_file_tree_from_santa_fileinfo)
+                          EnrollmentSerializer, build_file_tree_from_santa_fileinfo)
 from .tasks import export_targets
 
 
@@ -42,6 +46,74 @@ class ConfigurationDetail(generics.RetrieveUpdateDestroyAPIView):
             raise ValidationError('This configuration cannot be deleted')
         else:
             return super().perform_destroy(instance)
+
+
+class EnrollmentList(generics.ListCreateAPIView):
+    """
+    List all Enrollments or create a new Enrollment
+    """
+    queryset = Enrollment.objects.all()
+    permission_classes = [DefaultDjangoModelPermissions]
+    serializer_class = EnrollmentSerializer
+
+
+class EnrollmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete an Enrollment instance.
+    """
+    queryset = Enrollment.objects.all()
+    permission_classes = [DefaultDjangoModelPermissions]
+    serializer_class = EnrollmentSerializer
+
+    def perform_destroy(self, instance):
+        if not instance.can_be_deleted():
+            raise ValidationError('This enrollment cannot be deleted')
+        else:
+            return super().perform_destroy(instance)
+
+
+class EnrollmentConfiguration(APIView):
+    """
+    base enrollment configuration class. To be subclassed.
+    """
+    permission_required = "santa.view_enrollment"
+    permission_classes = [DjangoPermissionRequired]
+    configuration = None
+
+    def get(self, request, *args, **kwargs):
+        enrollment = get_object_or_404(Enrollment, pk=kwargs["pk"])
+        if self.configuration == "configuration_plist":
+            filename, content = build_configuration_plist(enrollment)
+            content_type = "application/x-plist"
+        elif self.configuration == "configuration_profile":
+            filename, content = build_configuration_profile(enrollment)
+            content_type = "application/octet-stream"
+        else:
+            raise ValidationError("Unknown enrollment configuration response type: {}".format(self.configuration))
+
+        content_base64 = base64.b64encode(content)
+
+        return Response({
+            "configuration": self.configuration,
+            "filename": filename,
+            "content_type": content_type,
+            "base64_content_length": len(content_base64),
+            "base64_content": content_base64
+        })
+
+
+class EnrollmentConfigurationPlist(EnrollmentConfiguration):
+    """
+    Download enrollment plist file
+    """
+    configuration = "configuration_plist"
+
+
+class EnrollmentConfigurationProfile(EnrollmentConfiguration):
+    """
+    Download enrollment configuration_profile
+    """
+    configuration = "configuration_profile"
 
 
 class IngestFileInfo(APIView):
