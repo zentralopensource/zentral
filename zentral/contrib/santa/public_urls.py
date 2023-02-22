048de40e13001562ac2c16a808241952bfd157ca
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2023-02-22 18:07:17 -0300

    Adding prefix to santa endpoints (cont.)

--- zentral/contrib/santa/public_urls.py
+++ zentral/contrib/santa/public_urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from . import views
+from . import public_views
 
 app_name = "santa_public"
 urlpatterns = [
         # API
     path('sync/<str:enrollment_secret>/preflight/<str:machine_id>',
-         csrf_exempt(views.PreflightView.as_view()), name='preflight'),
+         csrf_exempt(public_views.PreflightView.as_view()), name='preflight'),
     path('sync/<str:enrollment_secret>/ruledownload/<str:machine_id>',
-         csrf_exempt(views.RuleDownloadView.as_view()), name='ruledownload'),
+         csrf_exempt(public_views.RuleDownloadView.as_view()), name='ruledownload'),
     path('sync/<str:enrollment_secret>/eventupload/<str:machine_id>',
-         csrf_exempt(views.EventUploadView.as_view()), name='eventupload'),
+         csrf_exempt(public_views.EventUploadView.as_view()), name='eventupload'),
     path('sync/<str:enrollment_secret>/postflight/<str:machine_id>',
-         csrf_exempt(views.PostflightView.as_view()), name='postflight'),
+         csrf_exempt(public_views.PostflightView.as_view()), name='postflight'),
 ]
