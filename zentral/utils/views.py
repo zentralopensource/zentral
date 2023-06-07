5d67184c3b4b44d4ec851ecdfe5e35254c77125e
Éric Falconnier <eric.falconnier@112hz.com>
2023-06-07 15:50:31 +0200

    Better base audit views
    Build the even after the transaction is committed to get the latest
    state.

--- zentral/utils/views.py
+++ zentral/utils/views.py
@@ -6,11 +6,15 @@ from zentral.core.events.base import AuditEvent
 class CreateViewWithAudit(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
-        event = AuditEvent.build_from_request_and_instance(
-            self.request, self.object,
-            action=AuditEvent.Action.CREATED,
-        )
-        transaction.on_commit(lambda: event.post())
+
+        def post_event():
+            event = AuditEvent.build_from_request_and_instance(
+                self.request, self.object,
+                action=AuditEvent.Action.CREATED,
+            )
+            event.post()
+
+        transaction.on_commit(lambda: post_event())
         return response
 
 
@@ -19,18 +23,23 @@ class UpdateViewWithAudit(UpdateView):
         obj = self.get_object()  # self.object is already updated
         prev_value = obj.serialize_for_event()
         response = super().form_valid(form)
-        event = AuditEvent.build_from_request_and_instance(
-            self.request, self.object,
-            action=AuditEvent.Action.UPDATED,
-            prev_value=prev_value
-        )
-        transaction.on_commit(lambda: event.post())
+
+        def post_event():
+            event = AuditEvent.build_from_request_and_instance(
+                self.request, self.object,
+                action=AuditEvent.Action.UPDATED,
+                prev_value=prev_value
+            )
+            event.post()
+
+        transaction.on_commit(lambda: post_event())
         return response
 
 
 class DeleteViewWithAudit(DeleteView):
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
+        # build the event before the object is deleted
         event = AuditEvent.build_from_request_and_instance(
             self.request, self.object,
             action=AuditEvent.Action.DELETED,
