604e489117c94265f45106084942eae985e5f1d2
Éric Falconnier <eric.falconnier@112hz.com>
2023-06-09 10:44:55 +0200

    Fix Intune initial migration

--- ee/zentral/contrib/intune/migrations/0001_initial.py
+++ ee/zentral/contrib/intune/migrations/0001_initial.py
@@ -2,7 +2,6 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-import uuid
 
 
 class Migration(migrations.Migration):
@@ -20,13 +19,15 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=256, unique=True)),
                 ('description', models.TextField(blank=True)),
-                ('tenant_id', models.CharField(max_length=256, unique=True, help_text='The microsoft Azure Tenant ID')),
-                ('client_id', models.UUIDField(default=uuid.uuid4, unique=True, help_text='The client ID of your app registration')),
+                ('tenant_id', models.CharField(max_length=256, unique=True,
+                                               help_text='The microsoft Azure Tenant ID')),
+                ('client_id', models.UUIDField(unique=True, help_text='The client ID of your app registration')),
                 ('client_secret', models.TextField(help_text='The client secret of your app registration')),
                 ('version', models.PositiveIntegerField(editable=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('business_unit', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='inventory.BusinessUnit')),
+                ('business_unit', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT,
+                                                    to='inventory.BusinessUnit')),
             ],
         ),
     ]
