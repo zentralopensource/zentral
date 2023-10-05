57cac6233e8c4f61f90e193c5655c74eb3f29e25
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2023-10-05 12:28:29 +0200

    feat: Create Edit Delete Events Buttons as inclusion tags

--- server/base/templatetags/inclusion_tags.py
+++ server/base/templatetags/inclusion_tags.py
@@ -43,3 +43,47 @@ def to_str(value):
 @register.filter
 def to_int(value):
     return int(value)
+
+
+# Buttons
+
+ACTION = {
+    'CREATE': {
+        'icon': "bi bi-plus-circle",
+        'tooltip': "Create",
+    },
+    'UPDATE': {
+        'icon': "bi bi-pencil-square",
+        'tooltip': "Edit",
+    },
+    'DELETE': {
+        'icon': "bi bi-trash",
+        'tooltip': "Delete",
+    },
+    'EVENTS': {
+        'icon': "bi bi-activity",
+        'tooltip': "Browse the Events",
+    },
+}
+
+
+@register.inclusion_tag('_button.html')
+def button(action, url, tooltip=None, placement="bottom"):
+    """ Displays a Button.
+
+    Args:
+        action(str): kind of operation to perform (ACTION.keys())
+        url (str): a reversible url like "application:action".
+        tooltip (string, optional): A tooltip to display. If None, is not displayed.
+            Defaults to ACTION[key]['icon'].
+        placement (str, optional): Tooltip placement. Defaults to "bottom".
+    """
+    if action not in ACTION.keys():
+        raise ValueError("Wrong action name. Possible names are %s " % (str(ACTION.keys())))
+
+    return {
+        'url': url,
+        'icon': ACTION[action]['icon'],
+        'tooltip': ACTION[action]['tooltip'] if tooltip is None else tooltip,
+        'placement': placement,
+    }
