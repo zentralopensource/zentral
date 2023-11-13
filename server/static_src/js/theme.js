6a01bfb55a1f55d26f5ad7318769b8c901d47d6d
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2023-11-13 16:52:38 +0100

    fix: Theme Auto broken
    fix: PR comment addressed.

--- server/static_src/js/theme.js
+++ server/static_src/js/theme.js
@@ -13,39 +13,39 @@
         return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
       }
     const setTheme = theme => {
-        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.querySelectorAll("[data-bs-theme]").forEach(element => {
-                document.documentElement.setAttribute('data-bs-theme', 'dark')
-            })
+        if ( (theme === 'dark') || (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) ) {
             setDark()
-        } else {
-            document.querySelectorAll("[data-bs-theme]").forEach(element => {
-                element.setAttribute('data-bs-theme', theme)
-            })
-            if (theme === 'dark') {
-                setDark()
-            }
-            if (theme === 'light') {
-                setLight()
-            }
+        }
+        else {
+            setLight()
         }
     }
     const setDark = () => {
         document.querySelectorAll([".light", "[class*='-light']"]).forEach(element => {
-            element.className = element.className.replace("light","dark")
+            element.className = element.className.replace(" light"," dark")
+            element.className = element.className.replace("-light","-dark")
         })
         document.querySelectorAll([".text-dark"]).forEach(element => {
             element.classList.remove('text-dark')
             element.classList.add('text-light')
         })
+        document.querySelectorAll("[data-bs-theme]").forEach(element => {
+            element.setAttribute('data-bs-theme', 'dark')
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        })
     }
     const setLight = () => {
         document.querySelectorAll([".dark", "[class*='-dark']"]).forEach(element => {
-            element.className = element.className.replace("dark","light")
+            element.className = element.className.replace(" dark"," light")
+            element.className = element.className.replace("-dark","-light")
         })
         document.querySelectorAll([".text-light"]).forEach(element => {
             element.classList.remove('text-light')
             element.classList.add('text-dark')
+        })
+        document.querySelectorAll("[data-bs-theme]").forEach(element => {
+            element.setAttribute('data-bs-theme', 'light')
+            document.documentElement.setAttribute('data-bs-theme', 'light')
         })
     }
 
