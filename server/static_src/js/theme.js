043d2910ede40510f0facecbdb22167f42b2393f
Éric Falconnier <eric.falconnier@112hz.com>
2023-11-17 15:50:29 +0100

    Simplify and cleanup theme switching
     - Remove text-dark
     - Use only data attribute based theme switching
     - Fix active / hover / background colors for menu and btn-link

--- server/static_src/js/theme.js
+++ server/static_src/js/theme.js
@@ -14,39 +14,11 @@
       }
     const setTheme = theme => {
         if ( (theme === 'dark') || (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) ) {
-            setDark()
+          document.documentElement.setAttribute('data-bs-theme', 'dark')
         }
         else {
-            setLight()
+          document.documentElement.setAttribute('data-bs-theme', 'light')
         }
-    }
-    const setDark = () => {
-        document.querySelectorAll([".light", "[class*='-light']"]).forEach(element => {
-            element.className = element.className.replace(" light"," dark")
-            element.className = element.className.replace("-light","-dark")
-        })
-        document.querySelectorAll([".text-dark"]).forEach(element => {
-            element.classList.remove('text-dark')
-            element.classList.add('text-light')
-        })
-        document.querySelectorAll("[data-bs-theme]").forEach(element => {
-            element.setAttribute('data-bs-theme', 'dark')
-            document.documentElement.setAttribute('data-bs-theme', 'dark')
-        })
-    }
-    const setLight = () => {
-        document.querySelectorAll([".dark", "[class*='-dark']"]).forEach(element => {
-            element.className = element.className.replace(" dark"," light")
-            element.className = element.className.replace("-dark","-light")
-        })
-        document.querySelectorAll([".text-light"]).forEach(element => {
-            element.classList.remove('text-light')
-            element.classList.add('text-dark')
-        })
-        document.querySelectorAll("[data-bs-theme]").forEach(element => {
-            element.setAttribute('data-bs-theme', 'light')
-            document.documentElement.setAttribute('data-bs-theme', 'light')
-        })
     }
 
     const showActiveTheme = (theme, focus = false) => {
