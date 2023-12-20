553c1218e5db3dd88839e9868f8083fb594a25b7
Éric Falconnier <eric.falconnier@112hz.com>
2023-12-20 11:14:46 +0100

    Remove custom auth forms

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from accounts.views import login
-from accounts.forms import ZentralPasswordResetForm, ZentralPasswordChangeForm, ZentralSetPasswordForm
 from zentral.conf import settings as zentral_settings
 
 logger = logging.getLogger(__name__)
@@ -23,19 +22,22 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
     path('accounts/password_change/',
-         auth_views.PasswordChangeView.as_view(form_class=ZentralPasswordChangeForm),
+         auth_views.PasswordChangeView.as_view(),
          name='password_change'),
-    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+    path('accounts/password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
     path('accounts/password_reset/',
-         auth_views.PasswordResetView.as_view(form_class=ZentralPasswordResetForm),
+         auth_views.PasswordResetView.as_view(),
          name='password_reset'),
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    path('accounts/password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(form_class=ZentralSetPasswordForm),
+         auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+    path('accounts/reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
 ]
 
