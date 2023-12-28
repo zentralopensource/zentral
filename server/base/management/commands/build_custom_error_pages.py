e0999a9a67ef32250a7078c5d44dd247692cdd7d
Éric Falconnier <eric.falconnier@112hz.com>
2023-12-28 09:13:02 +0100

    Use full URL for custom error page assets
    To make them work even with mTLS errors

--- server/base/management/commands/build_custom_error_pages.py
+++ server/base/management/commands/build_custom_error_pages.py
@@ -3,6 +3,7 @@ import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.template.loader import get_template
+from zentral.conf import settings as zentral_settings
 
 
 logger = logging.getLogger("zentral.server.base.management.commands.build_custom_error_pages")
@@ -24,8 +25,10 @@ class Command(BaseCommand):
         template = get_template("custom_error_page.html")
         basedir = os.path.join(settings.STATIC_ROOT, "custom_error_pages")
         os.makedirs(basedir, exist_ok=True)
+        fqdn = zentral_settings["api"]["fqdn"]
         for status_code, message in self.errors:
             page_content = template.render({
+                "fqdn": fqdn,
                 "status_code": status_code,
                 "message": message,
             })
