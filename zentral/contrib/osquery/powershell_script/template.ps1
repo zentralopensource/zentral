19b8f6e9a8085b682bd2d46536529cbef206ca45
Éric Falconnier <eric.falconnier@112hz.com>
2024-02-15 14:35:39 +0100

    Modernize the Osquery enrollment powershell script

--- zentral/contrib/osquery/powershell_script/template.ps1
+++ zentral/contrib/osquery/powershell_script/template.ps1
@@ -1,29 +1,32 @@
-# UTF8 encoding without BOM
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+# make shure the Osquery folder exists
+New-Item -Force -Path "C:\Program Files\" -Name "osquery" -ItemType Directory
 
 # tls server cert
-$tls_server_certs_file = '%TLS_SERVER_CERTS_FILE%'
+$tls_server_certs_file = "%TLS_SERVER_CERTS_FILE%"
 $tls_server_certs = @"
 %TLS_SERVER_CERTS%
 "@
 if (%INCLUDE_TLS_SERVER_CERTS%) {
-  [System.IO.File]::WriteAllLines($tls_server_certs_file, $tls_server_certs, $Utf8NoBomEncoding)
+  Set-Content -Force -Path $tls_server_certs_file -Value $tls_server_certs
 }
 
 # enrollment secret
-$enrollment_secret_file = 'C:\Program Files\osquery\enrollment_secret.txt'
+$enrollment_secret_file = "C:\Program Files\osquery\enrollment_secret.txt"
 $enrollment_secret = "%ENROLL_SECRET_SECRET%"
-[System.IO.File]::WriteAllLines($enrollment_secret_file, $enrollment_secret, $Utf8NoBomEncoding)
+Set-Content -Force -Path $enrollment_secret_file -Value $enrollment_secret
 
 # flags
-$flags_file = 'C:\Program Files\osquery\osquery.flags'
+$flags_file = "C:\Program Files\osquery\osquery.flags"
 $flags = @"
 --enroll_secret_path=$enrollment_secret_file
 %EXTRA_FLAGS%
 "@
-[System.IO.File]::WriteAllLines($flags_file, $flags, $Utf8NoBomEncoding)
+Set-Content -Force -Path $flags_file -Value $flags
 
 # restart osqueryd service
-Stop-Service osqueryd
-Start-Sleep -s 2
-Start-Service osqueryd
+if(Get-Service -Name osqueryd -ErrorAction SilentlyContinue) {
+  Write-Host "Restart osqueryd" -ForegroundColor green
+  Restart-Service -Force -Name osqueryd
+} else {
+  Write-Host "Service osqueryd not found" -ForegroundColor red
+}
