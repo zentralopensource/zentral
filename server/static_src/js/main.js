886e9e4bf27e8befd2815b24df86561dc94bc563
Éric Falconnier <eric.falconnier@112hz.com>
2024-06-10 16:12:47 +0200

    Update chart.js to v4 and bundle it

--- server/static_src/js/main.js
+++ server/static_src/js/main.js
@@ -6,10 +6,16 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 // Enable bootstrap tooltip if the device doesn't have touch events.
-
 if(!('ontouchstart' in window)) {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
         trigger : 'hover'
     }))
 }
+
+// import Bar chart components of chart.js
+// see https://www.chartjs.org/docs/latest/getting-started/integration.html#bundle-optimization
+import { Chart, BarController, BarElement, LinearScale, CategoryScale } from 'chart.js'
+Chart.register(BarController, BarElement, LinearScale, CategoryScale)
+// TODO: find better solution!
+window.Chart = Chart;
