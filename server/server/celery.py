fbb89f22caf540737db230f1fdd8b58287a36141
Éric Falconnier <eric@zentral.com>
2025-01-20 22:07:43 +0100

    Fix celery deprecation warning

--- server/server/celery.py
+++ server/server/celery.py
@@ -3,6 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 app = Celery('zentral')
+app.conf.broker_connection_retry_on_startup = True
 app.conf.result_extended = True
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
