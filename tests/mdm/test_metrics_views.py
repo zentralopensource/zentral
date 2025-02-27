e639843c69f727c174fa67c0167d9e953c6d5e55
Éric Falconnier <eric@zentral.com>
2025-02-27 17:40:44 +0100

    Add MDM commands metrics

--- tests/mdm/test_metrics_views.py
+++ tests/mdm/test_metrics_views.py
@@ -4,7 +4,11 @@ from django.utils.crypto import get_random_string
 from prometheus_client.parser import text_string_to_metric_families
 from zentral.conf import settings
 from zentral.contrib.inventory.models import MetaBusinessUnit
-from .utils import force_dep_enrollment_session, force_ota_enrollment_session
+from zentral.contrib.mdm.artifacts import Target
+from zentral.contrib.mdm.commands import DeviceInformation, InstallProfile
+from zentral.contrib.mdm.models import Channel
+from .utils import (force_artifact, force_dep_enrollment_session,
+                    force_enrolled_user, force_ota_enrollment_session)
 
 
 @override_settings(
@@ -21,13 +25,10 @@ class MDMMetricsViewsTestCase(TestCase):
             HTTP_AUTHORIZATION=f'Bearer {settings["api"]["metrics_bearer_token"]}',
         )
 
-    def _assertSamples(self, families, samples):
+    def _assertSamples(self, families, samples, only_family=None):
         d = {}
-        families_to_ignore = []
         for family in families:
-            if family.type == "counter":
-                families_to_ignore.append(f"{family.name}_created")
-            if family.name in families_to_ignore:
+            if only_family and only_family != family.name:
                 continue
             sample_dict = d.setdefault(family.name, {})
             for sample in family.samples:
@@ -48,7 +49,7 @@ class MDMMetricsViewsTestCase(TestCase):
         response = self._make_authenticated_request()
         self.assertEqual(response.status_code, 200)
 
-    def test_metrics(self):
+    def test_enrollment_sessions_metrics(self):
         mbu = MetaBusinessUnit.objects.create(name=get_random_string(12))
         force_dep_enrollment_session(mbu)
         dep_session, _, _ = force_dep_enrollment_session(mbu, authenticated=True, completed=True, realm_user=True)
@@ -64,5 +65,47 @@ class MDMMetricsViewsTestCase(TestCase):
                     ('realm', dep_realm.name, 'status', 'COMPLETED', 'type', 'DEP'): 1.0,
                     ('realm', ota_realm.name, 'status', 'AUTHENTICATED', 'type', 'OTA'): 1.0,
                 },
-            }
+            },
+            only_family="zentral_mdm_enrollment_sessions",
+        )
+
+    def test_commands_metrics(self):
+        mbu = MetaBusinessUnit.objects.create(name=get_random_string(12))
+        session, _, _ = force_dep_enrollment_session(mbu, authenticated=True, completed=True)
+        a, (av,) = force_artifact()
+        cmd = InstallProfile.create_for_device(session.enrolled_device, av)
+        cmd.process_response(
+            {"Status": "Acknowledged"}, session, mbu
+        )
+        ua, (uav, _) = force_artifact(channel=Channel.USER, version_count=2)
+        eu = force_enrolled_user(session.enrolled_device)
+        cmd = InstallProfile.create_for_target(Target(session.enrolled_device, eu), uav)
+        cmd.process_response(
+            {"Status": "Error"}, session, mbu
+        )
+        session3, _, _ = force_dep_enrollment_session(mbu, authenticated=True, completed=True)
+        DeviceInformation.create_for_device(session3.enrolled_device)
+        response = self._make_authenticated_request()
+        self._assertSamples(
+            text_string_to_metric_families(response.content.decode("utf-8")),
+            {
+             'zentral_mdm_commands': {
+                 ('artifact', a.name,
+                  'channel', 'device',
+                  'name', 'InstallProfile',
+                  'status', 'Acknowledged',
+                  'version', '1'): 1.0,
+                 ('artifact', ua.name,
+                  'channel', 'user',
+                  'name', 'InstallProfile',
+                  'status', 'Error',
+                  'version', '2'): 1.0,
+                 ('artifact', '_',
+                  'channel', 'device',
+                  'name', 'DeviceInformation',
+                  'status', '_',
+                  'version', '_'): 1.0
+             }
+            },
+            only_family="zentral_mdm_commands"
         )
--- zentral/contrib/mdm/metrics_views.py
+++ zentral/contrib/mdm/metrics_views.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("zentral.core.mdm.metrics_views")
 
 class MetricsView(BasePrometheusMetricsView):
     def populate_enrollment_sessions(self):
-        isg = Counter(
+        esc = Counter(
             'zentral_mdm_enrollment_sessions', 'Zentral MDM enrollment sessions',
             ['type', 'status', 'realm'],
             registry=self.registry
@@ -31,11 +31,43 @@ class MetricsView(BasePrometheusMetricsView):
                 "group by aes.type, aes.status, r.name"
             )
             for type, status, realm, count in cursor.fetchall():
-                isg.labels(
+                esc.labels(
                     type=type,
                     status=status,
                     realm=realm or "_",
                 ).inc(count)
 
+    def populate_commands(self):
+        cc = Counter(
+            'zentral_mdm_commands', 'Zentral MDM commands',
+            ['channel', 'name', 'status', 'artifact', 'version'],
+            registry=self.registry
+        )
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "with agg_cmds as ("
+                "  select 'device' channel, name, status, artifact_version_id, count(*) count"
+                "  from mdm_devicecommand"
+                "  group by channel, name, status, artifact_version_id"
+                "  union"
+                "  select 'user' channel, name, status, artifact_version_id, count(*) count"
+                "  from mdm_usercommand"
+                "  group by channel, name, status, artifact_version_id"
+                ") "
+                "select acs.channel, acs.name, acs.status, a.name, av.version, acs.count "
+                "from agg_cmds acs "
+                "left join mdm_artifactversion av on (acs.artifact_version_id = av.id) "
+                "left join mdm_artifact a on (a.id = av.artifact_id)"
+            )
+            for channel, name, status, artifact, version, count in cursor.fetchall():
+                cc.labels(
+                    channel=channel,
+                    name=name,
+                    status=status or "_",
+                    artifact=artifact or "_",
+                    version=str(version) if version is not None else "_"
+                ).inc(count)
+
     def populate_registry(self):
         self.populate_enrollment_sessions()
+        self.populate_commands()
