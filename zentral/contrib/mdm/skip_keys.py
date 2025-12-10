d9f24283aeed2855d940483991a3faa90e20fe9b
Éric Falconnier <eric@zentral.com>
2025-12-10 10:14:45 +0100

    Add supported OS versions to skip key labels

--- zentral/contrib/mdm/skip_keys.py
+++ zentral/contrib/mdm/skip_keys.py
@@ -13,7 +13,19 @@ def build_skippable_setup_panes():
     skipkeys = load_yaml()
     skippable_setup_panes = []
     for payloadkey in skipkeys["payloadkeys"]:
-        skippable_setup_panes.append((payloadkey["key"], payloadkey["title"]))
+        label_items = [payloadkey["title"]]
+        supported_os = []
+        for platform in sorted(payloadkey.get("supportedOS", {}).keys()):
+            try:
+                from_version = payloadkey["supportedOS"][platform]["introduced"]
+            except KeyError:
+                supported_os.append(platform)
+            else:
+                if from_version != 'n/a':
+                    supported_os.append(f"{platform} ≥ {from_version}")
+        if supported_os:
+            label_items.append(", ".join(supported_os))
+        skippable_setup_panes.append((payloadkey["key"], " ‐ ".join(label_items)))
     return skippable_setup_panes
 
 
