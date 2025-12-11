543e92abe1295204d97220ddb98d642eab4e6560
Sebastian Fuchs <sebastian@zentral.com>
2025-12-11 14:22:00 +0000

    Add command tests for 'import_mac_assignments' & 'debug_inventory_clients'

--- tests/inventory/test_management_commands.py
+++ tests/inventory/test_management_commands.py
@@ -1,7 +1,9 @@
 from io import StringIO
+from unittest import mock
 from unittest.mock import patch
 from django.core.management import call_command
 from django.test import TestCase
+from zentral.contrib.inventory.models import MACAddressBlockAssignment
 
 
 class InventoryManagementCommandsTest(TestCase):
@@ -41,3 +43,40 @@ class InventoryManagementCommandsTest(TestCase):
         result = out.getvalue()
         self.assertTrue(result.startswith("Download URL:"))
         self.assertTrue(result.endswith(".zip\n"))
+
+    # import mac assigment
+
+    def test_import_mac_assignments(self):
+        out = StringIO()
+        with mock.patch('requests.get') as mock_requests:
+            mock_resp = mock.Mock()
+            mock_resp.text = "Header1, Header2, Header3, Header4\none, two, three, four"
+            mock_requests.return_value = mock_resp
+
+            call_command('import_mac_assignments', stdout=out)
+            result = out.getvalue()
+            self.assertIn('Import https://standards.ieee.org', result)
+            self.assertEqual(1, MACAddressBlockAssignment.objects.count())
+
+    # Debug inventory clients
+
+    def test_debug_inventory_clients_list(self):
+        out = StringIO()
+        call_command('debug_inventory_clients', '--list-clients', stdout=out)
+        result = out.getvalue()
+        self.assertIn('Configured clients:', result)
+        self.assertIn('key:', result)
+
+    def test_debug_inventory_clients_client(self):
+
+        out = StringIO()
+        call_command('debug_inventory_clients', '--client', 0, stdout=out)
+        result = out.getvalue()
+        self.assertIn('2 MACHINES', result)
+        self.assertIn('zentral.contrib.inventory.client', result)
+
+    def test_debug_inventory_clients_client_serial(self):
+        out = StringIO()
+        call_command('debug_inventory_clients', '--client', 0, '--serial-number', '0123456789', stdout=out)
+        result = out.getvalue()
+        self.assertIn("'serial_number': '0123456789',", result)
