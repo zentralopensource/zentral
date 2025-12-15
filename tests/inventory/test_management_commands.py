327c1ad9f328924f4e5d4b4b30c26f730f2fcd10
sebastian-zentral <sebastian@zentral.com>
2025-12-15 12:09:24 +0100

    Add inventory command test & small fixes (#1320)

--- tests/inventory/test_management_commands.py
+++ tests/inventory/test_management_commands.py
@@ -1,7 +1,9 @@
 from io import StringIO
-from unittest.mock import patch
+from unittest import mock
+from unittest.mock import patch, MagicMock
 from django.core.management import call_command
 from django.test import TestCase
+from zentral.contrib.inventory.models import MACAddressBlockAssignment
 
 
 class InventoryManagementCommandsTest(TestCase):
@@ -41,3 +43,46 @@ class InventoryManagementCommandsTest(TestCase):
         result = out.getvalue()
         self.assertTrue(result.startswith("Download URL:"))
         self.assertTrue(result.endswith(".zip\n"))
+
+    # import mac assigment
+
+    def test_import_mac_assignments(self):
+        out = StringIO()
+        with mock.patch('requests.get') as mock_requests:
+            side_effects = [
+                ["Registry,Assignment,Organization Name,Organization Address",
+                 'MA-L,286FB9,"Nokia Shanghai Bell Co., Ltd.","No.388 Ning Qiao Road,Jin Qiao Pudong Shanghai Shanghai   CN 201206 "'],
+                ["Registry,Assignment,Organization Name,Organization Address",
+                 'MA-M,C85CE27,SYNERGY SYSTEMS AND SOLUTIONS,"A1526, GREEN FIELDS COLONY Faridabad HARYANA IN 121001 "'],
+                ["Registry,Assignment,Organization Name,Organization Address",
+                 'MA-S,8C1F64AFA,"DATA ELECTRONIC DEVICES, INC",32 NORTHWESTERN DR SALEM NH US 03079']
+                 ]
+            mock_resp = MagicMock()
+            mock_resp.text.splitlines.side_effect = side_effects
+            mock_requests.return_value = mock_resp
+
+            call_command('import_mac_assignments', '--verbosity', 0, stdout=out)
+            self.assertEqual(3, MACAddressBlockAssignment.objects.count())
+
+    # Debug inventory clients
+
+    def test_debug_inventory_clients_list(self):
+        out = StringIO()
+        call_command('debug_inventory_clients', '--list-clients', stdout=out)
+        result = out.getvalue()
+        self.assertIn('Configured clients:', result)
+        self.assertIn('key:', result)
+
+    def test_debug_inventory_clients_client(self):
+
+        out = StringIO()
+        call_command('debug_inventory_clients', '--client', 0, stdout=out)
+        result = out.getvalue()
+        self.assertIn('2 MACHINES', result)
+        self.assertIn('zentral.contrib.inventory.client', result)
+
+    def test_debug_inventory_clients_client_serial(self):
+        out = StringIO()
+        call_command('debug_inventory_clients', '--client', 0, '--serial-number', '0123456789', stdout=out)
+        result = out.getvalue()
+        self.assertIn("'serial_number': '0123456789',", result)
