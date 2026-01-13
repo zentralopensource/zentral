ee0d74556a3b65ac2bc0945334383ec831de67b8
Sebastian Fuchs <sebastian@zentral.com>
2026-01-13 11:06:14 +0000

    Add new ztl token to api authentication

--- server/accounts/api_authentication.py
+++ server/accounts/api_authentication.py
@@ -2,10 +2,14 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions
 from rest_framework.authentication import TokenAuthentication
 from .models import APIToken
+from zentral.utils.token import verify_ztl_token, USER_API_TOKEN, SERVICE_ACCOUNT_API_TOKEN
 
 
 class APITokenAuthentication(TokenAuthentication):
     def authenticate_credentials(self, key):
+        # TODO: remove _ check in 2026.4
+        if '_' in key and not verify_ztl_token(key, [USER_API_TOKEN, SERVICE_ACCOUNT_API_TOKEN]):
+            raise exceptions.AuthenticationFailed(_('Invalid ztl token.'))
         try:
             token = APIToken.objects.get_with_key(key)
         except APIToken.DoesNotExist:
