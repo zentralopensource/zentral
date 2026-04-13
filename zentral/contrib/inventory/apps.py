d48db183ced614f67e296b9f7f3dd8b9e7aebec6
Éric Falconnier <eric@zentral.com>
2026-04-13 22:42:26 +0200

    Remove MetaBusinessUnitTag

--- zentral/contrib/inventory/apps.py
+++ zentral/contrib/inventory/apps.py
@@ -17,7 +17,6 @@ class ZentralInventoryAppConfig(ZentralAppConfig):
         "machinesnapshot",
         "machinetag",
         "metabusinessunit",
-        "metabusinessunittag",
         "osxapp",
         "osxappinstance",
         "program",
